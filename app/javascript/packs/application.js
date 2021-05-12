637d8d41044fe9a420e14f88773fac32f711ee3b
naohiro <your.email@kosuda0428@icloud.com>
2021-05-12 13:32:03 +0900

    [Add]Vuetify

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,23 @@
 import Vue from 'vue'
 import App from '../app.vue'
 
+// Vuetify
+import Vuetify from "vuetify"
+import "vuetify/dist/vuetify.min.css"
+import '@mdi/font/css/materialdesignicons.css'
+Vue.use(Vuetify)
+const vuetify = new Vuetify({
+  icons: {
+    iconfont: "mdi"
+  }
+})
+
 Vue.config.productionTip = false
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     el: "#app",
+    vuetify,
     render: h => h(App)
   })
   app.$mount("#app")
