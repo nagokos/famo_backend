026aad9f039c490f664a517707f566b27cad3bb7
naohiro <your.email@kosuda0428@icloud.com>
2021-05-12 13:46:56 +0900

    [Add]VueRouterの設定

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from '../app.vue'
 
+// Vue-router
+import VueRouter from "vue-router"
+import router from "./router"
+Vue.use(VueRouter)
+
 // Vuetify
 import Vuetify from "vuetify"
 import "vuetify/dist/vuetify.min.css"
@@ -18,6 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     el: "#app",
     vuetify,
+    router,
     render: h => h(App)
   })
   app.$mount("#app")
