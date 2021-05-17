37e71ec1878efc5bd58577f2fda0c63875ff5a8c
naohiro <your.email@kosuda0428@icloud.com>
2021-05-17 17:38:27 +0900

    [Add]新規登録の処理

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,20 @@
 class Api::V1::UsersController < ApplicationController
-  def create; end
+  def create
+    user = User.new(user_params)
+    if user.save
+      render head :ok
+    else
+      render json: user.errors, status: :bad_request
+    end
+  end
+
+  def update
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :avatar, :email, birth_date, :role, :introduction,
+                                 :password, :password_confirmation)
+  end
 end
