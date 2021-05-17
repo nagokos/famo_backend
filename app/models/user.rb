a9322d52fd461038905516a576132a10f69b5633
naohiro <your.email@kosuda0428@icloud.com>
2021-05-17 16:49:48 +0900

    [Add]Validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  validates :first_name, presence: true, length: { maximum: 30 }
+  validates :last_name, presence: true, length: { maximum: 30 }
+  validates :avatar, presence: true
+  validates :birth_date, presence: true
+  validates :role, presence: true
+  validates :introduction, length: { maximum: 10_000 }
+  validates :email, uniqueness: true, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})\z/i }
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+
+  enum role: { reviewer: 0, player: 1, admin: 2 }
 end
