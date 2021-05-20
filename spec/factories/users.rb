757d392780d87206ea36cd0c17ee090ec36110b9
naohiro <your.email@kosuda0428@icloud.com>
2021-05-21 00:54:30 +0900

    [Add]user_data

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :user do
-    
+    sequence(:first_name, 'first_name_1')
+    sequence(:last_name, 'last_name_1')
+    birth_date { '2000-01-01' }
+    role { 0 }
+    introduction { '' }
+    sequence(:email, 'rails_1@example.com')
+    password { 'foobar' }
+    password_confirmation {'foobar'}
   end
 end
