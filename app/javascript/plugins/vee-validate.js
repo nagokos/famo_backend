ae6ec15f85caa356bae22f65857a3db573db7067
naohiro <your.email@kosuda0428@icloud.com>
2021-05-22 22:07:46 +0900

    [Improve]フォーマットのバリデーションは一つのメソッドに

--- app/javascript/plugins/vee-validate.js
+++ app/javascript/plugins/vee-validate.js
@@ -1,16 +1,11 @@
 import { ValidationObserver, ValidationProvider, extend, setInteractionMode } from 'vee-validate'
-import { required, max, min, email } from 'vee-validate/dist/rules';
+import { required, max, min } from 'vee-validate/dist/rules';
 
 setInteractionMode("eager");
 
 extend("required", {
   ...required,
   message: "{_field_}を入力してください"
-})
-
-extend("email", {
-  ...email,
-  message: "{_field_}を正しく入力してください"
 })
 
 extend("max", {
@@ -23,9 +18,10 @@ extend("min", {
   message: "{_field_}は{length}文字以上で入力してください"
 })
 
-extend("birthDateFormat", {
-  validate(value) {
-    return value.match(/\d{4}-\d{2}-\d{2}/)
+extend("formFormat", {
+  params: ['format'],
+  validate(value, { format }) {
+    return value.match(format)
   },
   message: "入力形式が正しくありません"
 })
