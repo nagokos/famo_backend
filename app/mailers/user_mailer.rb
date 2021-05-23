c0388541fdf5d30e53e754cfb275d05dd39dfe9d
naohiro <your.email@kosuda0428@icloud.com>
2021-05-23 22:29:47 +0900

    [Fix]不要な記述を削除

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,7 @@
 class UserMailer < ApplicationMailer
   def activation_needed_email
-    @greeting = 'Hi'
-
-    mail to: 'to@example.org'
   end
 
   def activation_success_email
-    @greeting = 'Hi'
-
-    mail to: 'to@example.org'
   end
 end
