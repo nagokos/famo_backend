d895062b30142997ccea3d0a096f0278a5fba16d
naohiro <your.email@kosuda0428@icloud.com>
2021-05-23 17:43:15 +0900

    [Add]UserMailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,13 @@
+class UserMailer < ApplicationMailer
+  def activation_needed_email
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  def activation_success_email
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
