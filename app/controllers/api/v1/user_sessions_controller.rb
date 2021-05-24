00d40944059466028996e6747a6b09acde8c6d60
naohiro <your.email@kosuda0428@icloud.com>
2021-05-25 00:17:21 +0900

    [Fix]rubocopの指摘修正[条件式と同時に変数代入するようなとき]

--- app/controllers/api/v1/user_sessions_controller.rb
+++ app/controllers/api/v1/user_sessions_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::UserSessionsController < Api::V1::BaseController
   def create
-    user = User.authenticate(params[:email], params[:password])
-    if user
+    if (user = User.authenticate(params[:email], params[:password]))
       token = create_token(user)
       cookies[:token] = { value: token, expires: 1.month.from_now, secure: Rails.env.production?, httponly: true, same_site: 'Lax' }
       head :ok
