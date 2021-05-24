c9333fa0073ad3892957b76db1412a4c1c273a22
naohiro <your.email@kosuda0428@icloud.com>
2021-05-24 23:38:24 +0900

    [Add]activation_needed_email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,8 @@
 class UserMailer < ApplicationMailer
-  def activation_needed_email
+  def activation_needed_email(user)
+    @user = user
+    @url  = active_api_v1_user_url(@user.activation_token)
+    mail(to: user.email, subject: 'Welcome to My Awesome Site')
   end
 
   def activation_success_email
