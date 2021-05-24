4ab05e761bb4578c11413a8ee8eabe1ea699d382
naohiro <your.email@kosuda0428@icloud.com>
2021-05-25 04:53:50 +0900

    [Fix]rubocopの指摘修正(シングルクォートに)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   before_save :email_downcase
   before_create :set_activation_token_exp
   before_update :setup_activation, if: -> { email_changed? }
-  after_update :send_activation_needed_email!, if: -> { previous_changes["email"].present? }
+  after_update :send_activation_needed_email!, if: -> { previous_changes['email'].present? }
 
   authenticates_with_sorcery!
 
