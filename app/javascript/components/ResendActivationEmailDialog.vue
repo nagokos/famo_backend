ddafe61dfe211862c70ba87fd2abb86cd8d03f57
naohiro <your.email@kosuda0428@icloud.com>
2021-05-27 01:49:01 +0900

    [Add]validationの実装

--- app/javascript/components/ResendActivationEmailDialog.vue
+++ app/javascript/components/ResendActivationEmailDialog.vue
@@ -9,29 +9,44 @@
       </v-card-title>
       <v-divider />
       <v-card-text class="pt-6 pb-0">
-        <v-container>
-          <v-row>
-            <v-col cols="12" class="pb-0">
-              <v-text-field
-                v-model="email"
-                outlined
-                label="メールアドレス"
-                dense
-                background-color="#ECEFF1"
-                required
-              />
-            </v-col>
-            <v-col align="right" cols="12" class="pt-0">
-              <v-btn
-                color="blue darken-1"
-                text
-                @click="sendEmail"
-              >
-                送信
-              </v-btn>
-            </v-col>
-          </v-row>
-        </v-container>
+        <ValidationObserver ref="observer" v-slot="{ handleSubmit }">
+          <v-form>
+            <v-container>
+              <v-row>
+                <v-col cols="12" class="pb-0">
+                  <ValidationProvider
+                    vid="email"
+                    :rules="{
+                      required: true,
+                      formFormat: /^[a-zA-Z0-9_+-]+(.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/i
+                     }"
+                    name="メールアドレス"
+                    v-slot="{ errors }"
+                  >
+                    <v-text-field
+                      v-model="email"
+                      outlined
+                      label="メールアドレス"
+                      dense
+                      background-color="#ECEFF1"
+                      required
+                      :error-messages="errors"
+                    />
+                  </ValidationProvider>
+                </v-col>
+                <v-col align="right" cols="12" class="pt-0">
+                  <v-btn
+                    color="blue darken-1"
+                    text
+                    @click="handleSubmit(sendEmail)"
+                  >
+                    送信
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-form>
+        </ValidationObserver>
       </v-card-text>
     </v-card>
   </v-dialog>
@@ -56,7 +71,9 @@ export default {
         })
         Object.assign(this.$data, this.$options.data())
       } catch(err) {
-        console.log(err.response);
+        this.$refs.observer.setErrors({
+          email: ["ユーザーが見つかりませんでした"]
+        })
       }
     }
   }
