0b506f6dba907f7fd7f97b187f4d59ac92e2031e
naohiro <your.email@kosuda0428@icloud.com>
2021-05-31 13:50:04 +0900

    [Fix]ログイン失敗時のテストを細分化

--- spec/requests/api/v1/user_sessions_spec.rb
+++ spec/requests/api/v1/user_sessions_spec.rb
@@ -3,13 +3,12 @@ require 'rails_helper'
 RSpec.describe "Api::V1::UserSessions", type: :request do
   describe "POST /api/v1/login" do
     let(:user) { create(:user) }
-    before do
-      user.activate!
-      @header = { 'X-Requested-With': 'XMLHttpRequest' }
-    end
+    before { @header = { 'X-Requested-With': 'XMLHttpRequest' } }
 
-    context 'パラメーターが妥当な場合' do
+    context '正常系' do
+      before { user.activate! }
       before { post '/api/v1/login', headers: @header, params: { email: user.email, password: 'Foobar0123' } }
+
       it '成功して２００を返す' do
         expect(response.status).to eq(200)
       end
@@ -23,14 +22,44 @@ RSpec.describe "Api::V1::UserSessions", type: :request do
       end
     end
 
-    context 'パラメータが不正な場合' do
-      before { post '/api/v1/login', headers: @header, params: { email: 'rails@example.com', password: 'Foobar0123' } }
-      it '失敗して４０１を返す' do
-        expect(response.status).to eq(401)
+    context '異常系' do
+      context 'メールアドレスが間違っている' do
+        before { post '/api/v1/login', headers: @header, params: { email: 'rails@example.com', password: 'Foobar0123' } }
+
+        it '４００を返す' do
+          expect(response.status).to eq(400)
+        end
+
+        it 'エラーメッセージを返す' do
+          expect(json['email']).to eq('メールアドレスが間違っています')
+        end
       end
 
-      it 'ログインできない' do
-        expect(logged_in?).to be_falsey
+      context 'ユーザーが有効ではない' do
+        before { post '/api/v1/login', headers: @header, params: { email: user.email, password: 'Foobar0123' } }
+
+        it '４００を返す' do
+          expect(response.status).to eq(400)
+        end
+
+        it 'jsonを返す' do
+          expect(json['key']).to eq('inactive')
+        end
+      end
+
+      context 'パスワードが間違っている' do
+        before do
+          user.activate!
+          post '/api/v1/login', headers: @header, params: { email: user.email, password: 'invalid0000' }
+        end
+
+        it '４００を返す' do
+          expect(response.status).to eq(400)
+        end
+
+        it 'エラーメッセージを返す' do
+          expect(json['password']).to eq('パスワードが間違っています')
+        end
       end
     end
   end
