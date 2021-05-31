ced9eb78e388827fa2aa3590dd6795bec7aa0bf6
naohiro <your.email@kosuda0428@icloud.com>
2021-05-31 13:49:27 +0900

    [Fix]ログイン失敗時の処理を細分化

--- app/controllers/api/v1/user_sessions_controller.rb
+++ app/controllers/api/v1/user_sessions_controller.rb
@@ -1,11 +1,20 @@
 class Api::V1::UserSessionsController < Api::V1::BaseController
   def create
-    if (user = User.authenticate(params[:email], params[:password]))
-      token = user.create_token
-      cookies[:token] = { value: token, expires: 1.month.from_now, secure: Rails.env.production?, httponly: true, same_site: 'Lax' }
-      head :ok
-    else
-      render json: { message: 'ログインに失敗しました' }, status: :unauthorized
+    User.authenticate(params[:email], params[:password]) do |user, failure_reason|
+      if user && !failure_reason.present?
+        token = user.create_token
+        cookies[:token] = { value: token, expires: 1.month.from_now, secure: Rails.env.production?, httponly: true, same_site: 'Lax' }
+        head :ok
+      else
+        case failure_reason
+        when :invalid_login
+          render json: { email: 'メールアドレスが間違っています' }, status: :bad_request
+        when :inactive
+          render json: { key: 'inactive' }, status: :bad_request
+        when :invalid_password
+          render json: { password: 'パスワードが間違っています' }, status: :bad_request
+        end
+      end
     end
   end
 
