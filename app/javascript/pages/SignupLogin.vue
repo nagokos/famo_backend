8fbc669270c8e8c9ee5c30ee1b18e1a96d9ff399
naohiro <your.email@kosuda0428@icloud.com>
2021-06-01 16:39:26 +0900

    [Remove]コンポーネントに分けたため不要なコードを削除

--- app/javascript/pages/SignupLogin.vue
+++ app/javascript/pages/SignupLogin.vue
@@ -16,53 +16,17 @@
         </v-col>
       </v-row>
     </v-container>
-    <the-login-help-dialog
-      ref="loginHelpDialog"
-    />
   </div>
 </template>
 
 <script>
 import Signup from "../components/Signup"
-import TheLoginHelpDialog from "../components/TheLoginHelpDialog"
+import Login from "../components/Login"
 
 export default {
   components: {
     Signup,
-    TheLoginHelpDialog
-  },
-  data() {
-    return {
-      active: true,
-      show: false,
-      email: "",
-      password: ""
-    }
-  },
-  methods: {
-    openRegister() {
-      this.$refs.signupDialog.open()
-    },
-    openActivationEmail() {
-      this.$refs.activationEmailDialog.open()
-    },
-    openLoginHelp() {
-      this.$refs.loginHelpDialog.open()
-    },
-    async sendLoginData() {
-      try {
-        await this.$axios.post("/api/v1/login", {
-          email: this.email,
-          password: this.password,
-        })
-        await this.$store.dispatch("user/getCurrentUserFromAPI")
-      } catch(err) {
-        if (err.response.data.key === 'inactive') {
-          return this.active = false
-        }
-        this.$refs.observer.setErrors(err.response.data)
-      }
-    }
+    Login
   },
 }
 </script>
