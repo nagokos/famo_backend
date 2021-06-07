90102b4d43f4b88599d95c1af2439cba07cd5e82
naohiro <your.email@kosuda0428@icloud.com>
2021-06-07 17:31:44 +0900

    [Add]Information.vue

---
+++ app/javascript/components/parts/Information.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <v-row>
+      <!-- 経歴 -->
+      <v-col cols="12">
+        <information-career />
+      </v-col>
+      <!-- つながり -->
+      <v-col cols="12">
+        <information-relation
+          :user="user"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import InformationCareer from "./InformationCareer"
+import InformationRelation from "./InformationRelation"
+
+export default {
+  components: {
+     InformationCareer,
+     InformationRelation
+  },
+  props: {
+    user: {
+      type: Object,
+      default: () => {},
+      required: true
+    }
+  }
+}
+</script>
