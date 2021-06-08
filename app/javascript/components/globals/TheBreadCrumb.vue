ccf64a9dda233bcec1bbaeaa4344d914055b1e81
naohiro <your.email@kosuda0428@icloud.com>
2021-06-08 19:43:44 +0900

    [Add]TheBreadCrumb.vue

---
+++ app/javascript/components/globals/TheBreadCrumb.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-breadcrumbs
+    :items="breadCrumbs"
+    class="pl-3 mx-auto"
+    style="max-width: 1050px"
+  >
+    <template #item="props">
+      <v-breadcrumbs-item
+        :to="props.item.to"
+        :disabled="props.item.disabled"
+      >
+        <span
+          v-if="props.item.to !== '/'"
+          :class="$route.path === props.item.to ? 'text-caption black--text font-weight-bold' : 'text-caption'"
+        >{{ props.item.text }}</span>
+        <span
+          v-else
+          class="text-caption"
+        ><v-icon
+          size="18"
+          class="mr-1"
+          style="position: relative; bottom: 2px;"
+        >mdi-home</v-icon>{{ props.item.text }}</span>
+      </v-breadcrumbs-item>
+    </template>
+    <template #divider>
+      <v-icon>mdi-chevron-right</v-icon>
+    </template>
+  </v-breadcrumbs>
+</template>
+
+<script>
+export default {
+  props: {
+    breadCrumbs: {
+      type: Array,
+      default: () => {},
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+ .v-breadcrumbs
+ >>>
+ .v-breadcrumbs__divider {
+   padding: 0;
+ }
+</style>
