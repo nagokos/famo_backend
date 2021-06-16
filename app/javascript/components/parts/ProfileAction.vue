e96fbe43acc143801bcf9c0f65a9506914f46815
naohiro <your.email@kosuda0428@icloud.com>
2021-06-16 22:41:28 +0900

    [Add]ボタンクリックで親に伝達

--- app/javascript/components/parts/ProfileAction.vue
+++ app/javascript/components/parts/ProfileAction.vue
@@ -2,12 +2,12 @@
   <div :style="$vuetify.breakpoint.mobile ? 'position: relative; top: 27px;' : ''">
     <!-- ログインユーザーの場合 -->
     <v-btn
-      v-if="!$vuetify.breakpoint.mobile"
       rounded
       class="font-weight-bold px-2 py-5 text-caption"
       outlined
       x-large
       color="primary"
+      @click="$emit('click-edit')"
     >
       <v-icon>
         mdi-pencil-outline
