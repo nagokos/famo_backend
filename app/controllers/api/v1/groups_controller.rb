d362c32f9ec2867a33f9f615194d22259dde9d69
naohiro <your.email@kosuda0428@icloud.com>
2021-06-18 02:31:11 +0900

    [Fix]serializerを使用するため修正

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::GroupsController < ApplicationController
   def index
     groups = Group.all
-    render json: groups
+    render json: groups, each_serializer: GroupSerializer
   end
 end
