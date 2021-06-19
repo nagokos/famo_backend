d332d9a8459bd0d888073333d99ac4bcce5ebca0
naohiro <your.email@kosuda0428@icloud.com>
2021-06-20 04:26:39 +0900

    [Add]associationのテスト

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'association' do
+    it 'belongs_to :league' do
+      is_expected.to belong_to(:league)
+    end
+
+    it 'has_many :groups :restrict_with_exception' do
+      is_expected.to have_many(:groups).dependent(:restrict_with_exception)
+    end
+  end
 end
