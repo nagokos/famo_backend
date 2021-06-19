91c53a658712d7274eb3c6244e00ced509e5d44a
naohiro <your.email@kosuda0428@icloud.com>
2021-06-20 04:27:02 +0900

    [Add]associationのテスト　

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'association' do
+    it 'belongs_to :category' do
+      is_expected.to belong_to(:category)
+    end
+  end
 end
