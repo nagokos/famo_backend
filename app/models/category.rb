063c27abb07d616d7c2468b4882bc66fd1b7d949
naohiro <your.email@kosuda0428@icloud.com>
2021-06-19 11:25:18 +0900

    [Add]dependent: :restrict_with_exception

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
   belongs_to :league
-  has_many :groups
+  has_many :groups, dependent: :restrict_with_exception
 end
--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -1,3 +1,3 @@
 class Prefecture < ApplicationRecord
-  has_many :teams, dependent: :restrict_with_error
+  has_many :teams, dependent: :restrict_with_exception
 end
