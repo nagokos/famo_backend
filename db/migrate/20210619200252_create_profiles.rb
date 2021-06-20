72dc4b22050d4ffef0b711bd1d76c1ad65a626c9
naohiro <your.email@kosuda0428@icloud.com>
2021-06-21 01:56:40 +0900

    [Fix]nullを許容するように修正

--- db/migrate/20210619200252_create_profiles.rb
+++ db/migrate/20210619200252_create_profiles.rb
@@ -3,7 +3,7 @@ class CreateProfiles < ActiveRecord::Migration[6.1]
     create_table :profiles do |t|
       t.string :position, null: false
       t.integer :uniform_number, null: false
-      t.string :career, null: false
+      t.string :career
       t.references :user, null: false, fereign_key: true
       t.references :group, null: false, foreign_key: true
       t.references :team, null: false, foreign_key: true
