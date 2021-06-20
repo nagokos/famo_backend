9d45460422111c4794bfe87c263e35192513d250
naohiro <your.email@kosuda0428@icloud.com>
2021-06-21 05:07:51 +0900

    [Fix]返却されるエラーメッセージを修正

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -51,7 +51,7 @@ RSpec.describe 'Api::V1::Users', type: :request do
       it 'エラーメッセージを返す' do
         post '/api/v1/users', headers: @header, params: { user: { email: user.email } }
         expect(response.status).to eq(422)
-        expect(json['errors']).to include('メールアドレスはすでに存在します')
+        expect(json['errors']['email']).to include('このメールアドレスはすでに存在します')
       end
     end
   end
