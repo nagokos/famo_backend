47ca271290e4f60bb56e41f4b85b383c2f7b41be
naohiro <your.email@kosuda0428@icloud.com>
2021-06-21 01:37:02 +0900

    [Add]リーグからグループまでを取得できるように

--- app/serializers/league_serializer.rb
+++ app/serializers/league_serializer.rb
@@ -1,3 +1,13 @@
 class LeagueSerializer < ActiveModel::Serializer
   attributes :id, :name
+  has_many :categories
+
+  class CategorySerializer < ActiveModel::Serializer
+    attributes :id, :name
+    has_many :groups
+
+    class GroupSerializer < ActiveModel::Serializer
+      attributes :id, :name
+    end
+  end
 end
