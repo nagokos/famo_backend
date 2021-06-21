01e4cfa535b656c66d5f9569eeab8413cae47535
naohiro <your.email@kosuda0428@icloud.com>
2021-06-22 05:04:08 +0900

    [Remove]ファイル名変更に伴い削除

--- app/controllers/api/v1/prefectures_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::V1::PrefecturesController < ApplicationController
-  def index
-    prefectures = Prefecture.preload(:teams)
-    render json: prefectures, each_serializer: PrefectureSerializer
-  end
-end
