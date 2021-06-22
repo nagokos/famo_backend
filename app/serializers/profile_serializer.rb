ce918391b6da365ec3f0ec92e18be6afbd6eb799
naohiro <your.email@kosuda0428@icloud.com>
2021-06-23 06:22:15 +0900

    [Add]ネストしたデータを取得できるように

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,3 +1,28 @@
 class ProfileSerializer < ActiveModel::Serializer
   attributes :id, :position, :official_number, :practice_number, :team_id, :group_id
+  belongs_to :team
+  belongs_to :group
+
+  class TeamSerializer < ActiveModel::Serializer
+    attributes :name
+    belongs_to :prefecture
+
+    class PrefectureSerializer < ActiveModel::Serializer
+      attributes :name
+    end
+  end
+
+  class GroupSerializer < ActiveModel::Serializer
+    attributes :name
+    belongs_to :category
+
+    class CategorySerializer < ActiveModel::Serializer
+      attributes :name
+      belongs_to :league
+
+      class LeagueSerializer < ActiveModel::Serializer
+        attributes :name
+      end
+    end
+  end
 end
