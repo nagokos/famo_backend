c1aff0786812c12fedc4717eabf3dc87e3811587
naohiro <your.email@kosuda0428@icloud.com>
2021-06-24 07:06:52 +0900

    [Fix]ダイアログオープンをメソッドに

--- app/javascript/components/pages/Mypage.vue
+++ app/javascript/components/pages/Mypage.vue
@@ -9,7 +9,7 @@
       :user="currentUser"
       :user-edit="userEdit"
       :profile="profile"
-      @click-player="$refs.playerDialog.open()"
+      @click-player="openPlayerDialog"
       @click-edit="openEditDialog"
       @click-introduction="userEdit = { ...currentUser }"
       @click-update="updateIntroduction"
@@ -125,6 +125,9 @@ export default {
     openEditDialog() {
       this.setUserEdit()
       this.$refs.profileEditDialog.open()
+    },
+    openPlayerDialog() {
+      this.$refs.playerDialog.open()
     },
     async getProfileData() {
       const response = await this.$axios.get("/api/v1/profile")
