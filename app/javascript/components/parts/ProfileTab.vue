1b98bb8f02f4bcfc5847857adfc080c99e782284
naohiro <your.email@kosuda0428@icloud.com>
2021-06-24 17:45:56 +0900

    [Add]ProfileTab.vue

---
+++ app/javascript/components/parts/ProfileTab.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-tabs
+    color="black"
+    :fixed-tabs="$vuetify.breakpoint.mobile"
+    :class="$vuetify.breakpoint.mobile ? 'px-4' : ''"
+    background-color="#FAFAFA"
+  >
+    <v-tab
+      class="mr-5 font-weight-bold"
+      @click="$emit('click-user')"
+    >
+      ユーザー情報
+    </v-tab>
+    <v-tab
+      class="font-weight-bold"
+      @click="$emit('click-review')"
+    >
+      レビュー一覧
+    </v-tab>
+  </v-tabs>
+</template>
