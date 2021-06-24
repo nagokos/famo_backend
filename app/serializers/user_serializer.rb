4a024d2c6dced92a0c13f0b2b22b64963d142d84
naohiro <your.email@kosuda0428@icloud.com>
2021-06-25 07:10:25 +0900

    [Fix]不要なコードを削除

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,12 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :avatar, :birth_date, :role, :introduction,
-             :email, :activation
+             :email, :activation_state
 
   def role
     object.role_i18n
-  end
-
-  def activation
-    object.activation_state == 'active'
   end
 end
