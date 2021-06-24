93783e813ac7bca0b8a8a137dbbda163012fe5eb
naohiro <your.email@kosuda0428@icloud.com>
2021-06-24 22:18:48 +0900

    [Add]vidを追加

--- app/javascript/components/parts/ProfileIntroduction.vue
+++ app/javascript/components/parts/ProfileIntroduction.vue
@@ -42,6 +42,7 @@
                 v-slot="{ errors }"
                 rules="max:400"
                 name="自己紹介"
+                vid="introduction"
               >
                 <v-textarea
                   :value="introduction"
