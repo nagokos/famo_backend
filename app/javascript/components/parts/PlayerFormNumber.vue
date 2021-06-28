78eb8fa5e64cb200e35ddbd61c634badf03c305d
naohiro <your.email@kosuda0428@icloud.com>
2021-06-28 13:47:32 +0900

    [Add]PlayerFormNumber.vue

---
+++ app/javascript/components/parts/PlayerFormNumber.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-col cols="12">
+    <span
+      class="font-weight-bold text-h6 black--text"
+    >
+      背番号編集
+    </span>
+    <ValidationProvider
+      v-slot="{ errors }"
+      rules="required|numeric"
+      name="公式戦"
+      vid="official_number"
+    >
+      <v-text-field
+        :value="officialNumber"
+        class="mt-4"
+        label="公式戦"
+        background-color="#F2F4F8"
+        outlined
+        dense
+        required
+        hint="公式戦での背番号を入力して下さい"
+        persistent-hint
+        type="number"
+        :error-messages="errors"
+        @input="$emit('update:officialNumber', $event)"
+      />
+    </ValidationProvider>
+    <ValidationProvider
+      rules="numeric"
+      name="練習試合"
+      vid="practice_number"
+    >
+      <v-text-field
+        :value="practiceNumber"
+        class="mt-1"
+        label="練習試合"
+        outlined
+        dense
+        hint="練習試合での背番号を入力して下さい"
+        persistent-hint
+        type="number"
+        @input="$emit('update:practiceNumber', $event)"
+      />
+    </ValidationProvider>
+  </v-col>
+</template>
+
+<script>
+export default {
+  props: {
+    officialNumber: {
+      type: [Number, String],
+      default: "",
+      required: true
+    },
+    practiceNumber: {
+      type: [Number, String],
+      default: "",
+      required: false
+    }
+  }
+}
+</script>
