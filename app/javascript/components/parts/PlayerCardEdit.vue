8c1bfaa5ec6e0265e7ecad0d555d810b702e4b59
naohiro <your.email@kosuda0428@icloud.com>
2021-06-29 15:30:43 +0900

    [Fix]invalidに修正

--- app/javascript/components/parts/PlayerCardEdit.vue
+++ app/javascript/components/parts/PlayerCardEdit.vue
@@ -1,7 +1,7 @@
 <template>
   <ValidationObserver
     ref="observer"
-    v-slot="{ handleSubmit }"
+    v-slot="{ invalid }"
   >
     <v-card-text class="pb-0">
       <v-container>
@@ -33,14 +33,15 @@
       <v-btn
         color="primary"
         text
-        @click="clickCancel"
+        @click="$emit('click-cancel')"
       >
         キャンセル
       </v-btn>
       <v-btn
         color="primary"
         text
-        @click="handleSubmit(clickUpdate)"
+        :disabled="invalid"
+        @click="$emit('click-update')"
       >
         更新
       </v-btn>
