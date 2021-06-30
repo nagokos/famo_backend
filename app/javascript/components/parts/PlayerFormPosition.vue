739ed0a7575074631a1512111133659a26eefd3b
naohiro <your.email@kosuda0428@icloud.com>
2021-06-30 20:59:22 +0900

    [Remove]タイトルは含めないため削除

--- app/javascript/components/parts/PlayerFormPosition.vue
+++ app/javascript/components/parts/PlayerFormPosition.vue
@@ -1,10 +1,5 @@
 <template>
-  <v-col cols="12">
-    <span
-      class="font-weight-bold text-h6 black--text"
-    >
-      ポジション編集
-    </span>
+  <div>
     <ValidationProvider
       v-slot="{ errors }"
       rules="required"
@@ -23,7 +18,7 @@
         @change="$emit('update:position', $event)"
       />
     </ValidationProvider>
-  </v-col>
+  </div>
 </template>
 
 <script>
