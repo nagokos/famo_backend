a0a3bba8110a58ac8bb5bdcbb5c769f6661aebbd
naohiro <your.email@kosuda0428@icloud.com>
2021-07-03 18:22:10 +0900

    [Remove]不要なコードを削除

--- app/javascript/components/parts/PlayerCard.vue
+++ app/javascript/components/parts/PlayerCard.vue
@@ -3,36 +3,12 @@
     outlined
     elevation="1"
   >
-    <v-card-title
-      v-if="!profile"
-      class="font-weight-bold"
-    >
-      選手
-      <v-spacer />
-      <v-btn
-        color="primary"
-        text
-        @click="$emit('click-player')"
-      >
-        選手情報を追加
-      </v-btn>
-    </v-card-title>
     <div
       v-for="data in profileData"
-      v-else
       :key="data.id"
     >
       <v-card-title class="font-weight-bold">
         {{ data.name }}
-        <v-spacer />
-        <v-btn
-          icon
-          @click="$emit('click-edit', data.name)"
-        >
-          <v-icon>
-            mdi-pencil
-          </v-icon>
-        </v-btn>
       </v-card-title>
       <v-card-text
         align="center"
