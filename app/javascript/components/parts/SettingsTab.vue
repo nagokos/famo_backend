1e7d580f1cdfbc215d80ccda4fd3a68f86464a16
naohiro <your.email@kosuda0428@icloud.com>
2021-07-04 16:18:18 +0900

    [Add]SettingsTab.vue

---
+++ app/javascript/components/parts/SettingsTab.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-tabs
+    background-color="#FAFAFA"
+    @change="$emit('data-reset', $event)"
+  >
+    <v-tab
+      v-for="setting in settings"
+      :key="setting.params"
+      :to="{ name: 'mySetting', params: { type: setting.params } }"
+      class="font-weight-bold"
+    >
+      {{ setting.name }}
+    </v-tab>
+  </v-tabs>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      settings: [
+        {
+          name: "プロフィール",
+          params: "profile"
+        },
+        {
+          name: "選手登録",
+          params: "player"
+        }
+      ]
+    }
+  }
+}
+</script>
