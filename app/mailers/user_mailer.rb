7c516fc35414f1a72e42b44bf724e5f29bd970ac
naohiro <your.email@kosuda0428@icloud.com>
2021-07-07 00:19:14 +0900

    [Fix]subjectの修正

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,12 +2,12 @@ class UserMailer < ApplicationMailer
   def activation_needed_email(user)
     @user = user
     @url  = edit_api_v1_account_activation_url(@user.activation_token)
-    mail(to: user.email, subject: 'Welcome to My Awesome Site')
+    mail(to: user.email, subject: '認証メール送信のお知らせ')
   end
 
   def activation_success_email(user)
     @user = user
     @url = root_url
-    mail(to: user.email, subject: '認証に成功しました')
+    mail(to: user.email, subject: 'アカウント認証のお知らせ')
   end
 end
