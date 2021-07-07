262ee0ad2257e3641101f70360e27948db872e68
naohiro <your.email@kosuda0428@icloud.com>
2021-07-07 23:20:20 +0900

    [Fix]v-rippleをfalseに修正

--- app/javascript/components/parts/ProfileAction.vue
+++ app/javascript/components/parts/ProfileAction.vue
@@ -2,6 +2,7 @@
   <div :style="$vuetify.breakpoint.mobile ? 'position: relative; top: 27px;' : ''">
     <!-- ログインユーザーの場合 -->
     <v-btn
+      :ripple="false"
       class="font-weight-bold px-2 py-5 text-caption"
       large
       :to="{ name: 'mySetting', params: { type: 'profile' } }"
