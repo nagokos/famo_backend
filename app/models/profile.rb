fb73c51551d072bc137ac0e866178a3572334bd4
naohiro <your.email@kosuda0428@icloud.com>
2021-07-08 22:11:46 +0900

    [Fix]空文字が入ってくるので修正

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,7 +5,7 @@ class Profile < ApplicationRecord
 
   validates :position, presence: true
   validates :official_number, presence: true, numericality: true
-  validates :practice_number, numericality: true, allow_nil: true
+  validates :practice_number, numericality: true, allow_blank: true
   validates :career, length: { maximum: 50 }
 
   enum position: { GK: 0, DF: 1, MF: 2, FW: 3 }
