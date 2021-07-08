25138d74a55e4c6fcc33bfb45b2f19a8e59fed29
naohiro <your.email@kosuda0428@icloud.com>
2021-07-08 22:35:20 +0900

    [Add]validationを追加

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,7 +2,7 @@ class Team < ApplicationRecord
   belongs_to :prefecture
   has_many :profiles, dependent: :restrict_with_exception
 
-  validates :name, presence: true, uniqueness: { scope: :prefecture }
+  validates :name, presence: true, length: { maximum: 50 }, uniqueness: { scope: :prefecture }
   validates :kind, presence: true
 
   enum kind: { high_school: 0, youth: 1 }
