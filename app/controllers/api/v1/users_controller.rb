e818338ce97538453a10eabbf73d0da4ae2b7c04
naohiro <your.email@kosuda0428@icloud.com>
2021-07-12 05:35:43 +0900

    [Add]action show

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,6 +8,15 @@ class Api::V1::UsersController < Api::V1::BaseController
     end
   end
 
+  def show
+    user = User.find_by(id: params[:id])
+    if user
+      render json: user, status: :ok
+    else
+      head :not_found
+    end
+  end
+
   private
 
   def user_params
