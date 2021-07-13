14d4235db3f8c44e130a60e452f8c6f5df6092f8
naohiro <your.email@kosuda0428@icloud.com>
2021-07-13 09:23:26 +0900

    [Fix]lintの指摘修正

--- app/javascript/components/pages/User.vue
+++ app/javascript/components/pages/User.vue
@@ -5,7 +5,7 @@
     />
     <the-profile-wrapper
       :user="user"
-      :isFollow="isFollow"
+      :is-follow="isFollow"
       @click-follow="userFollow"
       @click-unfollow="userUnfollow"
     />
