1dc827a6daa88f4d01946619518b2331e9143f14
naohiro <your.email@kosuda0428@icloud.com>
2021-07-13 13:06:45 +0900

    [Add]method check_activation

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -8,4 +8,10 @@ class Api::V1::BaseController < ApplicationController
 
     render json: { error: 'forbidden' }, status: :forbidden
   end
+
+  def check_activation
+    return if current_user.activation_state == 'active'
+
+    render json: { message: 'アカウントを認証してください' }, status: :forbidden
+  end
 end
