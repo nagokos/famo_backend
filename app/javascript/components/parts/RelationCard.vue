f0a80baa1ea9aa2c9d91facbfa547585e3d85f78
naohiro <your.email@kosuda0428@icloud.com>
2021-07-18 17:12:01 +0900

    [Add]フォロー、フォロワーを実現するためのメソッドを追加

--- app/javascript/components/parts/RelationCard.vue
+++ app/javascript/components/parts/RelationCard.vue
@@ -76,9 +76,85 @@ export default {
       required: true
     }
   },
+  data() {
+    return {
+      loading: false,
+      following: [],
+      followers: [],
+      routes: [
+        {
+          name: "フォロー",
+          params: "following"
+        },
+        {
+          name: "フォロワー",
+          params: "followers"
+        }
+      ]
+    }
+  },
   computed: {
-    fullName() {
-      return `${this.user.last_name} ${this.user.first_name}`
+    isMypage() {
+      return this.$route.path.includes("/profile")
+    }
+  },
+  mounted() {
+    if (!this.$route.path.includes("/following")) {
+      this.$router.push({ name: this.setName, params: { type: "following" } })
+    }
+    this.setFollowing()
+  },
+  methods: {
+    followerCheck(id) {
+      if (this.followers.length !== 0) {
+        const index = this.$refs.followerComp.findIndex(el => {
+          return el.user.id === id
+        })
+        if (index !== -1) {
+          this.$refs.followerComp[index].switchFollow()
+        }
+      }
+    },
+    follwCheck(id) {
+      if (this.following.length !== 0) {
+        const index = this.$refs.followComp.findIndex(el => {
+          return el.user.id === id
+        })
+        if (index !== -1) {
+          this.$refs.followComp[index].switchFollow()
+        }
+      }
+    },
+    async setFollowing() {
+      await this.getFollowing()
+      await this.$refs.followComp[this.following.length - 1].checkFollow()
+      this.loading = true
+    },
+    async getFollowerData() {
+      if (this.$route.path.includes("/followers")) {
+        this.loading = false
+        await this.getFollowers()
+        await this.$refs.followerComp[this.followers.length - 1].checkFollow()
+        this.loading = true
+      }
+    },
+    async getFollowing() {
+      if (this.isMypage) {
+        const response = await this.$axios.get("/api/v1/users/current/following")
+        this.following = response.data.users
+      } else {
+        const response = await this.$axios.get(`/api/v1/users/${this.$route.params.userId}/following`)
+        this.following = response.data.users
+      }
+    },
+    async getFollowers() {
+      if (this.isMypage) {
+        const response = await this.$axios.get("/api/v1/users/current/followers")
+        this.followers = response.data.users
+      } else {
+        const response = await this.$axios.get(`/api/v1/users/${this.$route.params.userId}/followers`)
+        this.followers = response.data.users
+      }
     }
   }
 }
