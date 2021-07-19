a710d5126c5d20f9e70f7e2dbe7b8b2e9fbb5f04
naohiro <your.email@kosuda0428@icloud.com>
2021-07-19 13:02:20 +0900

    [Add]review.rb

---
+++ app/models/review.rb
@@ -0,0 +1,11 @@
+class Review < ApplicationRecord
+  belongs_to :reviewer, class_name: 'User'
+  belongs_to :reviewee, class_name: 'User'
+
+  validates :rate, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  validates :watching_date, format: { with: /\d{4}-\d{2}-\d{2}/ }, allow_blank: true
+  validates :privacy, presence: true
+  validates :content, presence: true, length: { maximum: 10_000 }
+
+  enum privacy: { published: 0, player_only: 1 }
+end
