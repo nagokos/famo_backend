6fc12f07489e239f2bf923a7aea0d30589a74a55
naohiro <your.email@kosuda0428@icloud.com>
2021-07-19 13:01:29 +0900

    [Add]has_manyを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,11 @@ class User < ApplicationRecord
 
   has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
-
   has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :follower
+
+  has_many :active_reviews, class_name: 'Review', foreign_key: 'reviewer_id', dependent: :nullify
+  has_many :passive_reviews, class_name: 'Review', foreign_key: 'reviewee_id', dependent: :nullify
 
   authenticates_with_sorcery!
 
