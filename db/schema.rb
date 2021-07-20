5e9ada99c22b08789645823f0c66641eca62cec2
naohiro <your.email@kosuda0428@icloud.com>
2021-07-20 14:34:48 +0900

    [Fix]カラムの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_18_113527) do
+ActiveRecord::Schema.define(version: 2021_07_20_043300) do
 
   create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -67,7 +67,7 @@ ActiveRecord::Schema.define(version: 2021_07_18_113527) do
 
   create_table "reviews", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.decimal "rate", precision: 2, scale: 1, null: false
-    t.date "watching_date"
+    t.date "game_date", null: false
     t.integer "privacy", default: 0, null: false
     t.text "content", null: false
     t.string "reviewer_id", null: false
