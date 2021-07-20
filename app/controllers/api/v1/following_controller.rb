d596acbf086a0d1bdd1a349bb96bde026b82ee27
naohiro <your.email@kosuda0428@icloud.com>
2021-07-20 21:23:08 +0900

    [Remove]ファイル構成変更のため削除

--- app/controllers/api/v1/following_controller.rb
+++
@@ -1,7 +0,0 @@
-class Api::V1::FollowingController < Api::V1::BaseController
-  def index
-    user = User.find(params[:user_id])
-    following = user.following
-    render json: following, each_serializer: RelationUserSerializer
-  end
-end
