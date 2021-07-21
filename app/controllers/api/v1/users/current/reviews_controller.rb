d0c9e10cc1e787744a603ea8cad233bb16286488
naohiro <your.email@kosuda0428@icloud.com>
2021-07-21 21:56:39 +0900

    [Fix]if分を修正

--- app/controllers/api/v1/users/current/reviews_controller.rb
+++ app/controllers/api/v1/users/current/reviews_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::Users::Current::ReviewsController < ApplicationController
   def index
-    if current_user.reviewer?
-      reviews = current_user.active_reviews
-    else
-      reviews = current_user.passive_reviews
-    end
+    reviews = if current_user.reviewer?
+                current_user.active_reviews
+              else
+                current_user.passive_reviews
+              end
     render json: reviews, role: current_user.role
   end
 end
