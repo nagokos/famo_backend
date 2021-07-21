1ff5b88d4e796ff8ec3ac65989aa61755106d9a9
naohiro <your.email@kosuda0428@icloud.com>
2021-07-21 23:48:37 +0900

    [Fix]ロジックをモデルに移動

--- app/controllers/api/v1/users/current/reviews_controller.rb
+++ app/controllers/api/v1/users/current/reviews_controller.rb
@@ -1,10 +1,6 @@
 class Api::V1::Users::Current::ReviewsController < ApplicationController
   def index
-    reviews = if current_user.reviewer?
-                current_user.active_reviews
-              else
-                current_user.passive_reviews
-              end
+    reviews = current_user.filter_reviews
     render json: reviews, role: current_user.role
   end
 end
