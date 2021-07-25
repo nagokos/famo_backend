d5312e81d697c6b9df327da00dd489e6ca761e20
naohiro <your.email@kosuda0428@icloud.com>
2021-07-25 13:39:13 +0900

    [Add]update and destroy

--- app/controllers/api/v1/users/current/reviews_controller.rb
+++ app/controllers/api/v1/users/current/reviews_controller.rb
@@ -4,6 +4,25 @@ class Api::V1::Users::Current::ReviewsController < Api::V1::BaseController
 
   def index
     reviews = current_user.filter_reviews.desc
-    render json: reviews, role: current_user.role
+    render json: reviews
+  end
+
+  def update
+    review = current_user.active_reviews.find(params[:id])
+    if review.update(review_params)
+      render json: review
+    else
+      render json: { errors: review.errors, message: "もう一度選択してください" }
+    end
+  end
+
+  def destroy
+    current_user.active_reviews.find(params[:id]).destroy!
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:privacy)
   end
 end
