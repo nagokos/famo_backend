723567ae8f77672f26d82f44e1da021d72f30eb8
naohiro <your.email@kosuda0428@icloud.com>
2021-07-25 13:38:54 +0900

    [Add]before_actionを追加

--- app/controllers/api/v1/users/current/reviews_controller.rb
+++ app/controllers/api/v1/users/current/reviews_controller.rb
@@ -1,4 +1,7 @@
-class Api::V1::Users::Current::ReviewsController < ApplicationController
+class Api::V1::Users::Current::ReviewsController < Api::V1::BaseController
+  before_action :required_login, only: %i[destroy]
+  before_action :check_activation, only: %i[destroy]
+
   def index
     reviews = current_user.filter_reviews.desc
     render json: reviews, role: current_user.role
