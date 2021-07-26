edd2e0a989d780034c1abe49ff4ffc73ab1d724b
naohiro <your.email@kosuda0428@icloud.com>
2021-07-26 20:25:52 +0900

    [Fix]rubocopの指摘修正(一行150文字まで)

--- app/controllers/api/v1/password_resets_controller.rb
+++ app/controllers/api/v1/password_resets_controller.rb
@@ -11,7 +11,8 @@ class Api::V1::PasswordResetsController < Api::V1::BaseController
     token = params[:id]
     user = User.load_from_reset_password_token(params[:id]) do |user, failure_reason|
       if user && !failure_reason
-        cookies[:reset_token] = { value: token, path: account_password_reset_path, expires: 5.minutes.from_now, secure: Rails.env.production?, same_site: 'Lax' }
+        cookies[:reset_token] = { value: token, path: account_password_reset_path,
+                                  expires: 5.minutes.from_now, secure: Rails.env.production?, same_site: 'Lax' }
         redirect_to account_password_reset_path
       else
         case failure_reason
