045d23d12f97437bf954dcffab13b57404809bc2
naohiro <your.email@kosuda0428@icloud.com>
2021-07-29 05:19:44 +0900

    [Add]not-found

--- app/javascript/store/index.js
+++ app/javascript/store/index.js
@@ -2,6 +2,7 @@ import Vue from "vue"
 import Vuex from "vuex"
 import user from "./user"
 import flash from "./flash"
+import notFound from "./not-found"
 
 Vue.use(Vuex)
 
@@ -9,5 +10,6 @@ export default new Vuex.Store({
   modules: {
     user,
     flash,
+    notFound
   }
 })
