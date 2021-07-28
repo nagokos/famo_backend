7ddd10d7dc38973bea3775b02e13781e8c483416
naohiro <your.email@kosuda0428@icloud.com>
2021-07-29 07:26:11 +0900

    [Fix]エンドポイントが長いため修正

--- config/routes.rb
+++ config/routes.rb
@@ -29,20 +29,27 @@ Rails.application.routes.draw do
           end
         end
       end
-      resources :leagues, only: %i[index show] do
-        resources :users, only: %i[index], module: :leagues
-        resources :categories, only: %i[show] do
-          resources :users, only: %i[index], module: :categories
-          resources :groups, only: %i[show] do
-            resources :users, only: %i[index], module: :groups
-          end
-        end
-      end
       resources :password_resets, only: %i[create edit update]
       resources :reviews, only: %i[index]
+
+      resources :leagues, only: %i[index show] do
+        resources :users, only: %i[index], module: :leagues
+      end
+
+      resources :categories, only: [] do
+        resources :users, only: %i[index], module: :categories
+      end
+
+      resources :groups, only: [] do
+        resources :users, only: %i[index], module: :groups
+      end
+
       resources :prefecture_teams, only: %i[index]
       resources :teams, only: %i[create]
       resources :account_activations, only: %i[create edit]
+
+      get ':league_id/categories/:id', to: 'categories#show'
+      get ':league_id/:category_id/groups/:id', to: 'groups#show'
       post '/login', to: 'user_sessions#create'
       delete '/logout', to: 'user_sessions#destroy'
     end
