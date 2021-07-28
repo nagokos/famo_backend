48dff0ca32ae193c98364bedf359ad6eedbd9a56
naohiro <your.email@kosuda0428@icloud.com>
2021-07-28 18:11:14 +0900

    [Add]アソシエーションを追加

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,3 +1,11 @@
 class GroupSerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :league
+  belongs_to :category
+
+  def league
+    {
+      id: object.category.league.id,
+      name: object.category.league.name
+    }
+  end
 end
