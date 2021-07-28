3e96b78613c06c77a8a1d0c030637123da5d54ed
naohiro <your.email@kosuda0428@icloud.com>
2021-07-29 07:29:54 +0900

    [Add]GroupPlayers.vue

---
+++ app/javascript/components/pages/GroupPlayers.vue
@@ -0,0 +1,67 @@
+<template>
+  <div
+    v-if="loading"
+    class="league"
+  >
+    <the-bread-crumb
+      :bread-crumbs="breadCrumbs"
+    />
+  </div>
+</template>
+
+<script>
+import Transform from "../../packs/league-transform"
+import TheBreadCrumb from "../globals/TheBreadCrumb"
+
+export default {
+  components: {
+    TheBreadCrumb
+  },
+  data() {
+    return {
+      group: {},
+      loading: false
+    }
+  },
+  computed: {
+    breadCrumbs() {
+      return [
+        {
+          text: "TOP",
+          to: "/",
+          disabled: false,
+        },
+        {
+          text: this.group.league.name,
+          to: `/${this.$route.params.league}`,
+          disabled: false,
+        },
+        {
+          text: this.group.category.name,
+          to: `/${this.$route.params.league}/${this.$route.params.categoryId}`,
+          disabled: false
+        },
+        {
+          text: this.group.name,
+          to: this.$route.path,
+          disabled: true
+        }
+      ]
+    }
+  },
+  created() {
+    this.getGroup()
+  },
+  methods: {
+    async getGroup() {
+      const leagueId = Transform.getLeagueId(this.$route.params.league)
+      const response = await this.$axios.get(
+        `/api/v1/${leagueId}/${this.$route.params.categoryId}/groups/${this.$route.params.groupId}`
+      )
+      this.group = response.data.group
+      this.loading = true
+      console.log(this.group);
+    }
+  }
+}
+</script>
