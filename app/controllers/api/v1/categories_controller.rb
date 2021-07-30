ccf0e9ae35c24b47cb1df28269df62aea52f407e
naohiro <your.email@kosuda0428@icloud.com>
2021-07-30 15:25:35 +0900

    [Fix]パスの整合性を確かめるために修正

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::CategoriesController < Api::V1::BaseController
   def show
-    category = Category.find(params[:id])
+    category = League.find(params[:league_id]).categories.find(params[:id])
     render json: category
   end
 end
