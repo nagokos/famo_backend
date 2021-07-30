b3b4a744185cc34064984311ff88b4ceff2e7ce5
naohiro <your.email@kosuda0428@icloud.com>
2021-07-30 15:26:09 +0900

    [Fix]パスの整合性を確かめるために修正

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::GroupsController < Api::V1::BaseController
   def show
-    group = Group.find(params[:id])
-    render json: group
+    users = League.find(params[:league_id]).categories.find(params[:category_id]).groups.find(params[:id])
+    render json: users
   end
 end
