ef93e00b7003a1415c3fb2ae3b85e2da1a3833f2
naohiro <your.email@kosuda0428@icloud.com>
2021-07-30 23:29:33 +0900

    [Add]index

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::CategoriesController < Api::V1::BaseController
+  def index; end
+
   def show
     category = League.find(params[:league_id]).categories.find(params[:id])
     render json: category
