4aecf3b878dea7ed7a444798672303585d86c6da
naohiro <your.email@kosuda0428@icloud.com>
2021-08-01 18:52:43 +0900

    [Add]index and show

--- app/controllers/api/v1/groups/users_controller.rb
+++ app/controllers/api/v1/groups/users_controller.rb
@@ -1,3 +1,11 @@
 class Api::V1::Groups::UsersController < Api::V1::BaseController
-  def index; end
+  def index
+    users = User.joins(profile: :group).merge(Group.where(id: params[:group_id]))
+    render json: users
+  end
+
+  def show
+    user = League.find(params[:league_id]).categories.find(params[:category_id]).groups.find(params[:group_id]).profiles.find_by(user_id: params[:id]).user
+    render json: user
+  end
 end
