c076cac4392b19a98231d29db8ad36f3268fb951
naohiro <your.email@kosuda0428@icloud.com>
2021-08-01 17:06:50 +0900

    [Fix]ロジックの修正

--- app/javascript/components/parts/RelationCard.vue
+++ app/javascript/components/parts/RelationCard.vue
@@ -50,8 +50,8 @@ export default {
   data() {
     return {
       loading: false,
-      following: [],
-      followers: [],
+      inactiveComp: "",
+      users: [],
       routes: [
         {
           name: "フォロー",
@@ -65,73 +65,67 @@ export default {
     }
   },
   computed: {
-    setName() {
-      return this.isMypage ? 'profile' : 'userProfile'
+    isFollowing() {
+      return this.$route.path.includes("/following")
     },
     isMypage() {
       return this.$route.path.includes("/profile")
-    }
+    },
   },
   created() {
-    if (!this.$route.path.includes("/following")) {
-      this.$router.push({ name: this.setName, params: { type: "following" } })
-    }
-    this.setFollowing()
+    this.getUsers()
+  },
+  mounted() {
+    this.$emit("reset-ids")
   },
   methods: {
-    followerCheck(id) {
-      if (this.followers.length !== 0) {
-        const index = this.$refs.followerComp.findIndex(el => {
-          return el.user.id === id
+    switchFollow(ids) {
+      if (!this.$refs.relationComp) return
+      if (ids.length === 0) return
+      let indexArray = []
+      ids.filter(comp => {
+        const index = this.$refs.relationComp.findIndex(el => {
+          return el.user.id === comp
         })
+        indexArray.push(index)
+      })
+      console.log(indexArray);
+      indexArray.forEach(index => {
         if (index !== -1) {
-          this.$refs.followerComp[index].switchFollow()
+          this.$refs.relationComp[index].switchFollow()
         }
-      }
+      })
+      this.$emit("reset-ids")
     },
-    follwCheck(id) {
-      if (this.following.length !== 0) {
-        const index = this.$refs.followComp.findIndex(el => {
-          return el.user.id === id
-        })
-        if (index !== -1) {
-          this.$refs.followComp[index].switchFollow()
-        }
-      }
+    checkFollow(id) {
+      this.$emit("check-follow", id)
     },
-    async setFollowing() {
-      await this.getFollowing()
-      if (this.following.length !== 0) {
-        await this.$refs.followComp[this.following.length - 1].checkFollow()
-      }
-      this.loading = true
-    },
-    async setFollowers() {
-      if (this.$route.path.includes("/followers")) {
-        this.loading = false
+    async getUsers() {
+      if (this.isFollowing) {
+        await this.getFollowing()
+      } else {
         await this.getFollowers()
-        if (this.followers.length !== 0) {
-          await this.$refs.followerComp[this.followers.length - 1].checkFollow()
-        }
-        this.loading = true
       }
+      if (!this.$refs.relationComp) return this.loading = true
+      await this.$refs.relationComp[this.$refs.relationComp.length - 1].checkFollow()
+      this.loading = true
     },
     async getFollowing() {
       if (this.isMypage) {
         const response = await this.$axios.get("/api/v1/users/current/following")
-        this.following = response.data.users
+        this.users = response.data.users
       } else {
         const response = await this.$axios.get(`/api/v1/users/${this.$route.params.userId}/following`)
-        this.following = response.data.users
+        this.users = response.data.users
       }
     },
     async getFollowers() {
       if (this.isMypage) {
         const response = await this.$axios.get("/api/v1/users/current/followers")
-        this.followers = response.data.users
+        this.users = response.data.users
       } else {
         const response = await this.$axios.get(`/api/v1/users/${this.$route.params.userId}/followers`)
-        this.followers = response.data.users
+        this.users = response.data.users
       }
     }
   }
