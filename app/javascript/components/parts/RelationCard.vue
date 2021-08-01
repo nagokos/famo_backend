c791606c9dc89c86a73643fb9faa436141f50213
naohiro <your.email@kosuda0428@icloud.com>
2021-08-01 17:03:13 +0900

    [Fix]UIの修正

--- app/javascript/components/parts/RelationCard.vue
+++ app/javascript/components/parts/RelationCard.vue
@@ -1,79 +1,43 @@
 <template>
-  <v-col cols="12">
-    <v-card outlined>
-      <v-col cols="12">
-        <v-card-title class="font-weight-bold text-h5 pb-0">
-          つながり
-        </v-card-title>
+  <v-container>
+    <v-row>
+      <template v-for="user in users">
+        <relation-card-item
+          v-show="loading"
+          ref="relationComp"
+          :key="user.id"
+          :user="user"
+          @check-follow="checkFollow"
+        />
+      </template>
+      <v-col
+        v-if="!loading"
+        cols="12"
+        align="center"
+      >
+        <v-progress-circular
+          indeterminate
+          color="primary"
+        />
       </v-col>
-      <v-card-text :class="$vuetify.breakpoint.mobile ? 'px-0 pt-0' : 'pt-0'">
-        <v-container>
-          <v-row>
-            <v-col cols="12">
-              <v-tabs
-                :grow="$vuetify.breakpoint.mobile"
-                :centered="$vuetify.breakpoint.mobile"
-              >
-                <v-tab
-                  v-for="route in routes"
-                  :key="route.params"
-                  :to="{ name: isMypage ? 'profile' : 'userProfile', params: { type: route.params } }"
-                  class="font-weight-bold"
-                  @click.once="setFollowers"
-                >
-                  {{ route.name }}
-                </v-tab>
-              </v-tabs>
-            </v-col>
-            <template v-for="follow in following">
-              <relation-card-item
-                v-show="$route.path.includes('/following') && loading"
-                ref="followComp"
-                :key="`follow-${follow.id}`"
-                :user="follow"
-                @check-follow="followerCheck(follow.id)"
-              />
-            </template>
-            <template v-for="follower in followers">
-              <relation-card-item
-                v-show="$route.path.includes('/followers') && loading"
-                ref="followerComp"
-                :key="`follower-${follower.id}`"
-                :user="follower"
-                @check-follow="follwCheck(follower.id)"
-              />
-            </template>
-            <v-col
-              v-if="!loading"
-              cols="12"
-              align="center"
-            >
-              <v-progress-circular
-                indeterminate
-                color="primary"
-              />
-            </v-col>
-            <v-col
-              v-if="loading && $route.path.includes('/following') && following.length === 0"
-              cols="12"
-              align="center"
-              class="text-h6"
-            >
-              フォローしていません
-            </v-col>
-            <v-col
-              v-if="loading && $route.path.includes('/followers') && followers.length === 0"
-              cols="12"
-              align="center"
-              class="text-h6"
-            >
-              フォロワーがいません
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card-text>
-    </v-card>
-  </v-col>
+      <v-col
+        v-if="loading && $route.path.includes('/following') && users.length === 0"
+        cols="12"
+        align="center"
+        class="text-h6"
+      >
+        フォローしていません
+      </v-col>
+      <v-col
+        v-if="loading && $route.path.includes('/followers') && users.length === 0"
+        cols="12"
+        align="center"
+        class="text-h6"
+      >
+        フォロワーはいません
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
