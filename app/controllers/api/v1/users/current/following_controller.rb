06e970690282ec41dcd224e523c381cb9815879c
naohiro <your.email@kosuda0428@icloud.com>
2021-08-03 17:20:09 +0900

    [Fix]bulletの指摘修正

--- app/controllers/api/v1/users/current/following_controller.rb
+++ app/controllers/api/v1/users/current/following_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Users::Current::FollowingController < Api::V1::BaseController
   def index
-    following = current_user.following
+    following = current_user.following.includes(profile: [team: :prefecture, group: { category: :league }])
     render json: following, each_serializer: RelationUserSerializer
   end
 end
