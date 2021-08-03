bbb326aa3b028df90b87bdd29e40e13d04d5a53d
naohiro <your.email@kosuda0428@icloud.com>
2021-08-03 18:08:58 +0900

    [Fix]scopeを使用

--- app/controllers/api/v1/users/following_controller.rb
+++ app/controllers/api/v1/users/following_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Users::FollowingController < Api::V1::BaseController
   def index
     user = User.find(params[:user_id])
-    following = user.following.includes(profile: [team: :prefecture, group: { category: :league }])
+    following = user.following.cache_profile
     render json: following, each_serializer: RelationUserSerializer
   end
 end
