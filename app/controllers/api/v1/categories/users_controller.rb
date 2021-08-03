b765f6a29726069c83127626b136397637fb2fc9
naohiro <your.email@kosuda0428@icloud.com>
2021-08-03 17:19:18 +0900

    [Fix]bulletの指摘修正

--- app/controllers/api/v1/categories/users_controller.rb
+++ app/controllers/api/v1/categories/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Categories::UsersController < Api::V1::BaseController
   def index
-    users = User.joins(profile: { group: :category }).merge(Category.where(id: params[:category_id]))
+    users = User.includes(profile: [team: :prefecture, group: { category: :league }]).joins(profile: { group: :category }).merge(Category.where(id: params[:category_id]))
     render json: users
   end
 end
