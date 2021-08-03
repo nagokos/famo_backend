4da29a09692b9b5b95beddbbda9347b7a0b14b5e
naohiro <your.email@kosuda0428@icloud.com>
2021-08-03 18:07:47 +0900

    [Fix]scopeを使用

--- app/controllers/api/v1/users/current/followers_controller.rb
+++ app/controllers/api/v1/users/current/followers_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Users::Current::FollowersController < Api::V1::BaseController
   def index
-    followers = current_user.followers.includes(profile: [team: :prefecture, group: { category: :league }])
+    followers = current_user.followers.cache_profile
     render json: followers, each_serializer: RelationUserSerializer
   end
 end
