a608562f20d7de031bc87aac8b6e2ddef322728a
naohiro <your.email@kosuda0428@icloud.com>
2021-08-03 18:07:16 +0900

    [Fix]rubocopの指摘修正

--- app/controllers/api/v1/groups/users_controller.rb
+++ app/controllers/api/v1/groups/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Groups::UsersController < Api::V1::BaseController
   def index
-    users = User.includes(profile: [team: :prefecture, group: { category: :league }]).joins(profile: :group).merge(Group.where(id: params[:group_id]))
+    users = User.cache_profile.joins(profile: :group).merge(Group.where(id: params[:group_id]))
     render json: users
   end
 end
