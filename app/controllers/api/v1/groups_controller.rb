3ec09eb6cfcd76d35548dfa1a5abbfb1ab190adb
naohiro <your.email@kosuda0428@icloud.com>
2021-08-04 17:54:25 +0900

    [Add]indexロジック

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::GroupsController < Api::V1::BaseController
-  def index; end
+  def index
+    categories = Group.includes(:category).where(category_id: params[:category_id])
+    render json: categories
+  end
 
   def show
     users = League.find(params[:league_id]).categories.find(params[:category_id]).groups.find(params[:id])
