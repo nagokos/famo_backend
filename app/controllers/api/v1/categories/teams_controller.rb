28c6707aab16f614eefa6bfa7dc5fcdf4844bb87
naohiro <your.email@kosuda0428@icloud.com>
2021-08-04 16:22:42 +0900

    [Add]teams_controller.rb

---
+++ app/controllers/api/v1/categories/teams_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::Categories::TeamsController < ApplicationController
+  def index
+    teams = Team.joins(profiles: { group: :category }).merge(Category.where(id: params[:category_id]))
+    render json: teams
+  end
+end
