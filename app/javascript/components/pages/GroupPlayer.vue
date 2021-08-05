8a7113c28295f0ae6280a0d9a976a81eac567e0d
naohiro <your.email@kosuda0428@icloud.com>
2021-08-06 04:12:42 +0900

    [Fix]コンポーネントでは扱いにくいため修正

--- app/javascript/components/pages/GroupPlayer.vue
+++ app/javascript/components/pages/GroupPlayer.vue
@@ -14,10 +14,47 @@
           :teams="teams"
           @search-player="searchPlayer"
         />
-        <player-list
-          :users="users"
-          :league="group"
-        />
+        <v-col
+          cols="12"
+          lg="8"
+        >
+          <div
+            class="font-weight-bold"
+            style="font-size: 1.8rem"
+          >
+            {{ group.name }}
+          </div>
+          <div v-if="$vuetify.breakpoint.mobile">
+            詳細条件
+          </div>
+          <v-tabs
+            class="mt-2"
+            background-color="#FAFAFA"
+            color="black"
+          >
+            <v-tab
+              exact
+              class="font-weight-bold"
+              :ripple="false"
+              :to="{ name: 'groupPlayer' }"
+            >
+              選手一覧
+            </v-tab>
+            <v-tab
+              exact
+              class="font-weight-bold"
+              :ripple="false"
+              :to="{ name: 'groupRating' }"
+            >
+              ランキング
+            </v-tab>
+          </v-tabs>
+          <v-divider />
+          <router-view
+            :users="users"
+            :league="group"
+          />
+        </v-col>
       </v-row>
     </v-container>
   </div>
