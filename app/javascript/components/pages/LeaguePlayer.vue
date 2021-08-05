465474994a3471e3a996337b01fffee639377ec2
naohiro <your.email@kosuda0428@icloud.com>
2021-08-06 04:19:17 +0900

    [Fix]コンポーネントでは扱いにくいため修正

--- app/javascript/components/pages/LeaguePlayer.vue
+++ app/javascript/components/pages/LeaguePlayer.vue
@@ -14,10 +14,47 @@
           :teams="teams"
           @search-player="searchPlayer"
         />
-        <player-list
-          :users="users"
-          :league="league"
-        />
+        <v-col
+          cols="12"
+          lg="8"
+        >
+          <div
+            class="font-weight-bold"
+            style="font-size: 1.8rem"
+          >
+            {{ league.name }}
+          </div>
+          <div v-if="$vuetify.breakpoint.mobile">
+            詳細条件
+          </div>
+          <v-tabs
+            class="mt-2"
+            background-color="#FAFAFA"
+            color="black"
+          >
+            <v-tab
+              exact
+              class="font-weight-bold"
+              :ripple="false"
+              :to="{ name: 'leaguePlayer' }"
+            >
+              選手一覧
+            </v-tab>
+            <v-tab
+              exact
+              class="font-weight-bold"
+              :ripple="false"
+              :to="{ name: 'leagueRating' }"
+            >
+              ランキング
+            </v-tab>
+          </v-tabs>
+          <v-divider />
+          <router-view
+            :users="users"
+            :league="league"
+          />
+        </v-col>
       </v-row>
     </v-container>
   </div>
