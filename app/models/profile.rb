85716a253898565be7eee03d65221fe015a6701a
naohiro <your.email@kosuda0428@icloud.com>
2021-08-06 03:54:09 +0900

    [Add]バリデーションを追加

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -7,6 +7,7 @@ class Profile < ApplicationRecord
   validates :official_number, presence: true, numericality: true
   validates :practice_number, numericality: true, allow_blank: true
   validates :career, length: { maximum: 50 }
+  validates :rate, presence: true
 
   enum position: { GK: 0, DF: 1, MF: 2, FW: 3 }
 end
