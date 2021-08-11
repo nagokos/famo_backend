136f7288d5fc0a80bea7e47f4cd356f2481f33f0
naohiro <your.email@kosuda0428@icloud.com>
2021-08-11 10:25:38 +0900

    [Add]search_reviews_form.rb

---
+++ app/forms/search_reviews_form.rb
@@ -0,0 +1,31 @@
+class SearchReviewsForm
+  include ActiveModel::Model
+  include ActiveModel::Attributes
+
+  attribute :sort, :string
+  attribute :game_date, :date
+
+  def search(user)
+    relation = user.filter_reviews
+
+    relation = sort_change(relation)
+    relation = relation.game_select_day(game_date) if game_date.present?
+
+    relation
+  end
+
+  private
+
+  def sort_change(relation)
+    case sort
+    when 'created'
+      relation.desc
+    when 'new_game'
+      relation.game_desc
+    when 'old_game'
+      relation.game_asc
+    when 'rating'
+      relation.rating
+    end
+  end
+end
