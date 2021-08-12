b5f3878469ec490c72285aa394cb7121744d27c5
naohiro <your.email@kosuda0428@icloud.com>
2021-08-13 08:47:24 +0900

    [Fix]継承を修正

--- app/controllers/api/v1/leagues/teams_controller.rb
+++ app/controllers/api/v1/leagues/teams_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::Leagues::TeamsController < ApplicationController
+class Api::V1::Leagues::TeamsController < Api::V1::BaseController
   def index
     teams = Team.joins(profiles: { group: { category: :league } }).merge(League.where(id: params[:league_id]))
     render json: teams
