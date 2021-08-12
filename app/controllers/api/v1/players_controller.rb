e161718f17bf7fd64a2780085321a604f6a4de6c
naohiro <your.email@kosuda0428@icloud.com>
2021-08-13 08:46:31 +0900

    [Fix]継承を修正

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::PlayersController < ApplicationController
+class Api::V1::PlayersController < Api::V1::BaseController
   def index
     search_users_form = SearchUsersForm.new(search_params)
     users = search_users_form.search
