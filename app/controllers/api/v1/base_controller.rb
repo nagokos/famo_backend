bf40af6389543004c8a3961a01e9511509a95ff5
naohiro <your.email@kosuda0428@icloud.com>
2021-08-13 16:55:27 +0900

    [Add]after_action

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::BaseController < ApplicationController
   before_action :check_xhr_header, only: %i[create update destroy]
+  after_action { pagy_headers_merge(@pagy) if @pagy }
 
   include Pagy::Backend
 
