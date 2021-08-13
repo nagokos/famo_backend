97df0e118259f33dec2571aa217e1f53098378a8
naohiro <your.email@kosuda0428@icloud.com>
2021-08-13 16:56:23 +0900

    [Fix]例外処理

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -2,6 +2,11 @@ class Api::V1::PlayersController < Api::V1::BaseController
   def index
     search_users_form = SearchUsersForm.new(search_params)
     users = search_users_form.search
+    begin
+      @pagy, users = pagy(users)
+    rescue Pagy::OverflowError => e
+      return head :not_found
+    end
     render json: users
   end
 
