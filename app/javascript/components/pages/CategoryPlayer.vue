ccfd87411d99887dc4848f6213cbd607c3bccc88
naohiro <your.email@kosuda0428@icloud.com>
2021-08-13 18:22:34 +0900

    [Fix]リロード時を想定して修正

--- app/javascript/components/pages/CategoryPlayer.vue
+++ app/javascript/components/pages/CategoryPlayer.vue
@@ -93,6 +93,7 @@ export default {
     }
   },
   created() {
+    if (!!this.$route.query.page) this.page = +this.$route.query.page
     if (!!this.$route.params.search) this.q = this.$route.params.search
     this.getData()
   },
