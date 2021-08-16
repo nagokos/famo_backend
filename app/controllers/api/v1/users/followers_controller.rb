9ba03c5bcaec470461e28628dba5e304b9703134
naohiro <your.email@kosuda0428@icloud.com>
2021-08-17 07:40:22 +0900

    [Add]pagy

--- app/controllers/api/v1/users/followers_controller.rb
+++ app/controllers/api/v1/users/followers_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::Users::FollowersController < Api::V1::BaseController
   def index
     user = User.find(params[:user_id])
     followers = user.followers.cache_profile
+    @pagy, followers = pagy(followers)
     render json: followers, each_serializer: RelationUserSerializer
   end
 end
