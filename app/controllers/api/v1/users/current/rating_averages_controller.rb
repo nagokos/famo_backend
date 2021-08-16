43e020a18467cf2d701e271cf59dc45bdb9e1061
naohiro <your.email@kosuda0428@icloud.com>
2021-08-16 09:29:44 +0900

    [Fix]rubocopの指摘修正

--- app/controllers/api/v1/users/current/rating_averages_controller.rb
+++ app/controllers/api/v1/users/current/rating_averages_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::Users::Current::RatingAveragesController < Api::V1::BaseController
   def show
     average = current_user.active_reviews.average(:rate)
-    if average.nil?
-      average = 0
-    else
-      average = format('%10.1f', average).strip
-    end
+    average = if average.nil?
+                0
+              else
+                format('%10.1f', average).strip
+              end
     render json: average
   end
 end
