6b44684e3800cb1bf0d79cf7c6b459824d38e248
naohiro <your.email@kosuda0428@icloud.com>
2021-08-25 16:01:37 +0900

    [Fix]カラムの変更に伴い修正

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -16,6 +16,6 @@ class Api::V1::UsersController < Api::V1::BaseController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :avatar, :email, :birth_date, :password)
+    params.require(:user).permit(:name, :avatar, :email, :birth_date, :password)
   end
 end
