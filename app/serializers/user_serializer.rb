d60c88c1f14a00350bd53cac0d2fcfccb8c59427
naohiro <your.email@kosuda0428@icloud.com>
2021-08-25 16:01:17 +0900

    [Fix]カラムの変更に伴い修正

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :avatar, :birth_date, :role, :introduction,
+  attributes :id, :name, :avatar, :birth_date, :role, :introduction,
              :email, :activation_state
   has_one :profile
 end
