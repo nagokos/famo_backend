308e165251d0f3074455749883d2012f008827c6
naohiro <your.email@kosuda0428@icloud.com>
2021-08-27 15:26:23 +0900

    [Fix]ruboocopの指摘修正

--- app/controllers/api/v1/oauths_controller.rb
+++ app/controllers/api/v1/oauths_controller.rb
@@ -5,41 +5,46 @@ class Api::V1::OauthsController < Api::V1::BaseController
 
   def callback
     provider = params[:provider]
-
     return redirect_to root_path if params[:error].present?
 
-    if user = login_from(provider)
-      token = user.create_token
-      cookies[:token] = { value: token, expires: 1.month.from_now, secure: Rails.env.production?, httponly: true, same_site: 'Lax' }
-      return redirect_to profile_path
-    end
-
-    user = build_from(provider)
-    user.authentications.build(provider: provider, uid: @user_hash[:uid])
-
-    if provider == 'line'
-      userData = JWT.decode(@access_token.params["id_token"], @provider.secret)
-      if userData.first['email'].present?
-        user.email = userData.first['email']
-      else
-        cookies[:error] = { value: 'email_blank', expires: 1.minutes.from_now }
-        return redirect_to login_path
-      end
-    end
-
-    if user.invalid? && user.errors.include?(:password) && !user.errors.include?(:email)
-      user.save(validate: false)
-      token = user.create_token
-      cookies[:create] = { value: 'signup_user', expires: 1.minutes.from_now }
-      cookies[:token] = { value: token, expires: 1.month.from_now, secure: Rails.env.production?, httponly: true, same_site: 'Lax' }
-      redirect_to root_path
+    if (user = login_from(provider))
+      cookies[:login] = { value: 1, expires: 1.minutes.from_now }
     else
-      if user.errors.of_kind?(:email, :taken)
-        cookies[:error] = { value: 'email_taken', expires: 1.minutes.from_now }
-        redirect_to login_path
-      else
-        redirect root_path
-      end
+      user = build_from(provider)
+      user.authentications.build(provider: provider, uid: @user_hash[:uid])
+      return redirect_to login_path if set_email_from_line(user, provider) || check_email(user)
+
+      user.save(validate: false)
+      cookies[:signup] = { value: 1, expires: 1.minutes.from_now }
     end
+    set_token(user)
+    redirect_to profile_path
+  end
+
+  private
+
+  def set_email_from_line(user, provider)
+    return unless provider == 'line'
+
+    decoded_id_token = JWT.decode(@access_token.params['id_token'], @provider.secret)
+    if decoded_id_token.first['email'].present?
+      user.email = decoded_id_token.first['email']
+      false
+    else
+      cookies[:email_blank] = { value: 1, expires: 1.minutes.from_now }
+      true
+    end
+  end
+
+  def check_email(user)
+    return unless User.exists?(email: user.email)
+
+    cookies[:email_taken] = { value: 1, expires: 1.minutes.from_now }
+    true
+  end
+
+  def set_token(user)
+    token = user.create_token
+    cookies[:token] = { value: token, expires: 1.month.from_now, secure: Rails.env.production?, httponly: true, same_site: 'Lax' }
   end
 end
