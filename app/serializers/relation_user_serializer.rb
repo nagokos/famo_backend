a6370a15027b041d0f1e4e73552c2d4fa9054755
naohiro <your.email@kosuda0428@icloud.com>
2021-08-27 15:38:11 +0900

    [Fix]名前のカラム変更に伴い修正　

--- app/serializers/relation_user_serializer.rb
+++ app/serializers/relation_user_serializer.rb
@@ -1,4 +1,4 @@
 class RelationUserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :avatar, :role
+  attributes :id, :name, :avatar, :role
   belongs_to :profile
 end
