d03da766cadf1f01283eab21d7cb4395a24bfe33
naohiro <your.email@kosuda0428@icloud.com>
2021-08-27 15:50:42 +0900

    [Fix]lineでログインクリック時に別のコンポーネントを表示できるよに

--- app/javascript/components/parts/SignupDialog.vue
+++ app/javascript/components/parts/SignupDialog.vue
@@ -9,12 +9,17 @@
     <signup-dialog-select
       v-if="registerSelect"
       @email-register="showForm"
+      @line-agree="lineAgree"
       @click-close="dialog = false"
     />
     <signup-dialog-form
       v-if="emailRegister"
       @create-user="showSendEmail"
       @click-back="closeForm"
+    />
+    <signup-dialog-line
+      v-if="line"
+      @click-back="closeLine"
     />
     <send-activation-email
       v-if="sendEmail"
@@ -28,17 +33,20 @@
 import SendActivationEmail from "./SendActivationEmail"
 import SignupDialogSelect from "./SignupDialogSelect"
 import SignupDialogForm from "./SignupDialogForm"
+import SignupDialogLine from "./SignupDialogLine"
 
 export default {
   components: {
     SendActivationEmail,
     SignupDialogSelect,
-    SignupDialogForm
+    SignupDialogForm,
+    SignupDialogLine
   },
   data() {
     return {
       dialog: false,
       registerSelect: false,
+      line: false,
       emailRegister: false,
       sendEmail: false,
       email: ""
@@ -53,8 +61,16 @@ export default {
       this.emailRegister = true
       this.registerSelect = false
     },
+    lineAgree() {
+      this.line = true
+      this.registerSelect = false
+    },
     closeForm() {
       this.emailRegister = false
+      this.registerSelect = true
+    },
+    closeLine() {
+      this.line = false
       this.registerSelect = true
     },
     showSendEmail(email) {
