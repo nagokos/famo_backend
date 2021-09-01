2c315c256440274c0a8252c4be6a95c3a9d19ead
naohiro <your.email@kosuda0428@icloud.com>
2021-09-01 20:26:10 +0900

    [Fix]修正

--- app/dashboards/profile_dashboard.rb
+++ app/dashboards/profile_dashboard.rb
@@ -76,7 +76,7 @@ class ProfileDashboard < Administrate::BaseDashboard
   # Overwrite this method to customize how profiles are displayed
   # across all pages of the admin dashboard.
   #
-  def display_resource(_profile)
+  def display_resource(profile)
     ''
   end
 end
