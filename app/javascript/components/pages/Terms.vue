54eeeb1c8b68c2b388c125096d06b7e05c307723
naohiro <your.email@kosuda0428@icloud.com>
2021-09-01 20:27:34 +0900

    [Fix]yarn run lint --fix

--- app/javascript/components/pages/Terms.vue
+++ app/javascript/components/pages/Terms.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="terms">
-    <v-card elevation="0" :class="$vuetify.breakpoint.mobile ? '' : 'mt-15 '" :color="$vuetify.breakpoint.mobile ? '#FAFAFA' : 'white'">
+    <v-card
+      elevation="0"
+      :class="$vuetify.breakpoint.mobile ? '' : 'mt-15 '"
+      :color="$vuetify.breakpoint.mobile ? '#FAFAFA' : 'white'"
+    >
       <v-card-title class="justify-center text-h5 font-weight-bold py-8">
         利用規約
       </v-card-title>
@@ -11,22 +15,40 @@
             <v-col cols="12">
               この利用規約（以下、「本規約」といいます。）は、本ウェブサイト上で提供するサービス（以下、「本サービス」といいます。）の利用条件を定めるものです。登録ユーザーの皆さま（以下、「ユーザー」といいます。）には、本規約に同意して頂いたうえで、本サービスをご利用いただきます。
             </v-col>
-            <v-col cols="12" :class="$vuetify.breakpoint.mobile ? 'text-h6 font-weight-bold mt-3' : 'text-h5 font-weight-bold mt-5'">
+            <v-col
+              cols="12"
+              :class="$vuetify.breakpoint.mobile ? 'text-h6 font-weight-bold mt-3' : 'text-h5 font-weight-bold mt-5'"
+            >
               第1条（適用）
             </v-col>
-            <v-col cols="12" class="pt-0">
+            <v-col
+              cols="12"
+              class="pt-0"
+            >
               本規約は、ユーザーと本サービス運営者（以下、「運営者」といいます。）との間の本サービスの利用に関わる一切の関係に適用されるものとします。
             </v-col>
-            <v-col cols=12 :class="$vuetify.breakpoint.mobile ? 'text-h6 font-weight-bold mt-3' : 'text-h5 font-weight-bold mt-5'">
+            <v-col
+              cols="12"
+              :class="$vuetify.breakpoint.mobile ? 'text-h6 font-weight-bold mt-3' : 'text-h5 font-weight-bold mt-5'"
+            >
               第2条（利用登録）
             </v-col>
-            <v-col cols="12" class="pt-0">
+            <v-col
+              cols="12"
+              class="pt-0"
+            >
               本サービスにおいては、登録希望者が本規約に同意の上、登録手続きを行うことができるものとします。
             </v-col>
-            <v-col cols=12 :class="$vuetify.breakpoint.mobile ? 'text-h6 font-weight-bold mt-3' : 'text-h5 font-weight-bold mt-5'">
+            <v-col
+              cols="12"
+              :class="$vuetify.breakpoint.mobile ? 'text-h6 font-weight-bold mt-3' : 'text-h5 font-weight-bold mt-5'"
+            >
               第3条（ユーザーIDおよびパスワードの管理）
             </v-col>
-            <v-col cols=12 class="pt-0">
+            <v-col
+              cols="12"
+              class="pt-0"
+            >
               <ol>
                 <li>
                   ユーザーは、自己の責任において、本サービスのユーザーIDおよびパスワードを適切に管理するものとします。
@@ -39,10 +61,16 @@
                 </li>
               </ol>
             </v-col>
-            <v-col cols=12 :class="$vuetify.breakpoint.mobile ? 'text-h6 font-weight-bold mt-3' : 'text-h5 font-weight-bold mt-5'">
+            <v-col
+              cols="12"
+              :class="$vuetify.breakpoint.mobile ? 'text-h6 font-weight-bold mt-3' : 'text-h5 font-weight-bold mt-5'"
+            >
               第4条（禁止事項）
             </v-col>
-            <v-col cols=12 class="pt-0">
+            <v-col
+              cols="12"
+              class="pt-0"
+            >
               ユーザーは、本サービスの利用にあたり、以下の行為をしてはなりません。
               <ol class="mt-4">
                 <li>
@@ -92,10 +120,16 @@
                 </li>
               </ol>
             </v-col>
-            <v-col cols=12 :class="$vuetify.breakpoint.mobile ? 'text-h6 font-weight-bold mt-3' : 'text-h5 font-weight-bold mt-5'">
+            <v-col
+              cols="12"
+              :class="$vuetify.breakpoint.mobile ? 'text-h6 font-weight-bold mt-3' : 'text-h5 font-weight-bold mt-5'"
+            >
               第5条（本サービスの提供の停止等）
             </v-col>
-            <v-col cols="12" class="pt-0">
+            <v-col
+              cols="12"
+              class="pt-0"
+            >
               運営者は、以下のいずれかの事由があると判断した場合、ユーザーに事前に通知することなく本サービスの全部または一部の提供を停止または中断することができるものとします。
               <ol class="mt-4 mb-4">
                 <li>
@@ -113,10 +147,16 @@
               </ol>
               運営者は、本サービスの提供の停止または中断により、ユーザーまたは第三者が被ったいかなる不利益または損害についても、一切の責任を負わないものとします。
             </v-col>
-            <v-col cols=12 :class="$vuetify.breakpoint.mobile ? 'text-h6 font-weight-bold mt-3' : 'text-h5 font-weight-bold mt-5'">
+            <v-col
+              cols="12"
+              :class="$vuetify.breakpoint.mobile ? 'text-h6 font-weight-bold mt-3' : 'text-h5 font-weight-bold mt-5'"
+            >
               第6条（利用制限および登録抹消）
             </v-col>
-            <v-col cols="12" class="pt-0">
+            <v-col
+              cols="12"
+              class="pt-0"
+            >
               運営者は、ユーザーが以下のいずれかに該当する場合には、事前の通知なく、ユーザーに対して、本サービスの全部もしくは一部の利用を制限し、またはユーザーとしての登録を抹消することができるものとします。
               <ol class="mt-4 mb-4">
                 <li>
@@ -131,16 +171,28 @@
               </ol>
               運営者は、本条に基づき運営者が行った行為によりユーザーに生じた損害について、一切の責任を負いません。
             </v-col>
-            <v-col cols=12 :class="$vuetify.breakpoint.mobile ? 'text-h6 font-weight-bold mt-3' : 'text-h5 font-weight-bold mt-5'">
+            <v-col
+              cols="12"
+              :class="$vuetify.breakpoint.mobile ? 'text-h6 font-weight-bold mt-3' : 'text-h5 font-weight-bold mt-5'"
+            >
               第7条（退会）
             </v-col>
-            <v-col cols="12" class="pt-0">
+            <v-col
+              cols="12"
+              class="pt-0"
+            >
               ユーザーは、いつでも本サービスから退会することができます。
             </v-col>
-            <v-col cols=12 :class="$vuetify.breakpoint.mobile ? 'text-h6 font-weight-bold mt-3' : 'text-h5 font-weight-bold mt-5'">
+            <v-col
+              cols="12"
+              :class="$vuetify.breakpoint.mobile ? 'text-h6 font-weight-bold mt-3' : 'text-h5 font-weight-bold mt-5'"
+            >
               第8条（保証の否認および免責事項）
             </v-col>
-            <v-col cols="12" class="pt-0">
+            <v-col
+              cols="12"
+              class="pt-0"
+            >
               <ol>
                 <li>
                   運営者は、本サービスに事実上または法律上の瑕疵（安全性、信頼性、正確性、完全性、有効性、特定の目的への適合性、セキュリティなどに関する欠陥、エラーやバグ、権利侵害などを含みます。）がないことを明示的にも黙示的にも保証しておりません。
@@ -150,31 +202,62 @@
                 </li>
               </ol>
             </v-col>
-            <v-col cols=12 :class="$vuetify.breakpoint.mobile ? 'text-h6 font-weight-bold mt-3' : 'text-h5 font-weight-bold mt-5'">
+            <v-col
+              cols="12"
+              :class="$vuetify.breakpoint.mobile ? 'text-h6 font-weight-bold mt-3' : 'text-h5 font-weight-bold mt-5'"
+            >
               第9条（サービス内容の変更等）
             </v-col>
-            <v-col cols="12" class="pt-0">
+            <v-col
+              cols="12"
+              class="pt-0"
+            >
               運営者は、ユーザーに通知することなく、本サービスの内容を変更しまたは本サービスの提供を中止することができるものとし、これによってユーザーに生じた損害について一切の責任を負いません。
             </v-col>
-            <v-col cols=12 :class="$vuetify.breakpoint.mobile ? 'text-h6 font-weight-bold mt-3' : 'text-h5 font-weight-bold mt-5'">
+            <v-col
+              cols="12"
+              :class="$vuetify.breakpoint.mobile ? 'text-h6 font-weight-bold mt-3' : 'text-h5 font-weight-bold mt-5'"
+            >
               第10条（利用規約の変更）
             </v-col>
-            <v-col cols="12" class="pt-0">
+            <v-col
+              cols="12"
+              class="pt-0"
+            >
               運営者は、必要と判断した場合には、ユーザーに通知することなくいつでも本規約を変更することができるものとします。なお、本規約の変更後、本サービスの利用を開始した場合には、当該ユーザーは変更後の規約に同意したものとみなします。
             </v-col>
-            <v-col cols=12 :class="$vuetify.breakpoint.mobile ? 'text-h6 font-weight-bold mt-3' : 'text-h5 font-weight-bold mt-5'">
+            <v-col
+              cols="12"
+              :class="$vuetify.breakpoint.mobile ? 'text-h6 font-weight-bold mt-3' : 'text-h5 font-weight-bold mt-5'"
+            >
               第11条（個人情報の取扱い）
             </v-col>
-            <v-col cols="12" class="pt-0">
-              運営者は、本サービスの利用によって取得する個人情報については、<span class="blue--text text--darken-2" style="cursor: pointer;" @click="$router.push({ name: 'privacy' }, () => {})">プライパシーポリシー</span>に従い適切に取り扱うものとします。
+            <v-col
+              cols="12"
+              class="pt-0"
+            >
+              運営者は、本サービスの利用によって取得する個人情報については、<span
+                class="blue--text text--darken-2"
+                style="cursor: pointer;"
+                @click="$router.push({ name: 'privacy' }, () => {})"
+              >プライパシーポリシー</span>に従い適切に取り扱うものとします。
             </v-col>
-            <v-col cols=12 :class="$vuetify.breakpoint.mobile ? 'text-h6 font-weight-bold mt-3' : 'text-h5 font-weight-bold mt-5'">
+            <v-col
+              cols="12"
+              :class="$vuetify.breakpoint.mobile ? 'text-h6 font-weight-bold mt-3' : 'text-h5 font-weight-bold mt-5'"
+            >
               第12条（通知または連絡）
             </v-col>
-            <v-col cols="12" class="pt-0">
+            <v-col
+              cols="12"
+              class="pt-0"
+            >
               ユーザーと運営者との間の通知または連絡は、運営者の定める方法によって行うものとします。
             </v-col>
-            <v-col align="end" cols="12">
+            <v-col
+              align="end"
+              cols="12"
+            >
               以上
             </v-col>
           </v-row>
