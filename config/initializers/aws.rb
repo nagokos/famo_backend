239b985a53fac55b8f74dbc26023380585e0e1ec
naohiro <your.email@kosuda0428@icloud.com>
2021-09-03 23:32:16 +0900

    [Fix]rubocopの指摘修正

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,6 @@
 Aws.config.update(
-  region: 'ap-northeast-1' ,
-  credentials: Aws::Credentials.new(Rails.application.credentials.aws.dig(:access_key_id), Rails.application.credentials.aws.dig(:secret_access_key))
+  region: 'ap-northeast-1',
+  credentials: Aws::Credentials.new(Rails.application.credentials.aws[:access_key_id], Rails.application.credentials.aws[:secret_access_key])
 )
 
 s3_resource = Aws::S3::Resource.new
