e63837e49095064b4ef2290d1ada9afe39781455
naohiro <your.email@kosuda0428@icloud.com>
2021-09-04 13:45:45 +0900

    [Fix]アバターのレスポンスを修正

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,12 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :avatar, :birth_date, :role, :introduction,
              :email, :activation_state
   has_one :profile
+
+  def avatar
+    if object.avatar.url
+      "https://#{Rails.application.credentials.aws[:bucket]}.s3.ap-northeast-1.amazonaws.com#{object.avatar.url}"
+    else
+      'https://pics.prcm.jp/a508a977c6fa9/84540173/png/84540173.png'
+    end
+  end
 end
