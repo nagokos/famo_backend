6ccefeb736198e8115af2feaeafe8087f96760c0
naohiro <your.email@kosuda0428@icloud.com>
2021-09-08 01:10:13 +0900

    [Fix]条件式の修正

--- app/controllers/api/v1/password_resets_controller.rb
+++ app/controllers/api/v1/password_resets_controller.rb
@@ -16,7 +16,7 @@ class Api::V1::PasswordResetsController < Api::V1::BaseController
         redirect_to account_password_reset_path
       else
         case failure_reason
-        when :invalid_token || :token_expired
+        when :invalid_token, :token_expired
           cookies[:password_reset] = { value: 1, expires: 1.minutes.from_now }
         when :user_not_found
           cookies[:password_reset] = { value: 2, expires: 1.minutes.from_now }
