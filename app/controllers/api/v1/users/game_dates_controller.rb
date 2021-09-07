b361816480af5d3c3782f656fb3ddfbd05d613bd
naohiro <your.email@kosuda0428@icloud.com>
2021-09-08 01:19:12 +0900

    [Fix]スコープ名変更に伴い修正

--- app/controllers/api/v1/users/game_dates_controller.rb
+++ app/controllers/api/v1/users/game_dates_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Users::GameDatesController < Api::V1::BaseController
   def index
     user = User.find(params[:user_id])
-    game_dates = user.filter_reviews_dates.everyone.map(&:game_date).map(&:strftime)
+    game_dates = user.filter_reviews_dates.published.map(&:game_date).map(&:strftime)
     render json: { reviews: game_dates }
   end
 end
