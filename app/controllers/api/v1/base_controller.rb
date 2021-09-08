37f79b562e516c2125af13a00739e3a0083aa13f
naohiro <your.email@kosuda0428@icloud.com>
2021-09-08 21:50:20 +0900

    [Fix]エラー処理をconcernに切り出し

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -3,18 +3,5 @@ class Api::V1::BaseController < ApplicationController
   after_action { pagy_headers_merge(@pagy) if @pagy }
 
   include Pagy::Backend
-
-  private
-
-  def check_xhr_header
-    return if request.xhr?
-
-    render json: { error: 'forbidden' }, status: :forbidden
-  end
-
-  def check_activation
-    return if current_user.activation_token.nil?
-
-    render json: { message: 'アカウントを認証してください' }, status: :forbidden
-  end
+  include ErrorHandle
 end
