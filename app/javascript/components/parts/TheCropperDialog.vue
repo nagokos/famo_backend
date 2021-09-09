d5846de940eae0b9377e53fb9109610c13e991db
naohiro <your.email@kosuda0428@icloud.com>
2021-09-09 19:46:19 +0900

    [Add]TheCropperDialog.vue

---
+++ app/javascript/components/parts/TheCropperDialog.vue
@@ -0,0 +1,113 @@
+<template>
+  <v-dialog
+    v-model="dialog"
+    width="400"
+    :fullscreen="$vuetify.breakpoint.mobile"
+    :transition="$vuetify.breakpoint.mobile ? 'dialog-bottom-transition' : 'dialog-transition'"
+    :persistent="true"
+  >
+    <v-card color="#FAFAFA">
+      <v-card-actions class="py-3">
+        <v-btn
+          icon
+          :ripple="false"
+          @click="close"
+        >
+          <v-icon>
+            mdi-close
+          </v-icon>
+        </v-btn>
+        <v-spacer />
+        <v-btn
+          color="#3949AB"
+          dark
+          rounded
+          depressed
+          width="80"
+          class="text-caption font-weight-bold"
+          @click="sizeApply"
+        >
+          適用する
+        </v-btn>
+      </v-card-actions>
+      <v-divider />
+      <v-card-text>
+        <vue-cropper
+          style="height: 400px;"
+          ref="cropper"
+          :img="blobUrl"
+          :autoCrop="true"
+        >
+        </vue-cropper>
+        <v-slider
+          class="mt-4"
+          v-model="size"
+          max="20"
+          @input="changeSize"
+          hide-details
+          append-icon="mdi-magnify-plus-outline"
+          prepend-icon="mdi-magnify-minus-outline"
+          @click:append="plusSize"
+          @click:prepend="minusSize"
+        />
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    blobUrl: {
+      type: String,
+      default: "",
+      required: true
+    },
+    fileName: {
+      type: String,
+      default: "",
+      required: true
+    }
+  },
+  data() {
+    return {
+      dialog: false,
+      size: 0,
+      oldSize: 0
+    }
+  },
+  methods: {
+    open() {
+      this.dialog = true
+    },
+    close() {
+      Object.assign(this.$data, this.$options.data())
+    },
+    changeSize() {
+      const size = this.size - this.oldSize
+      this.$refs.cropper.changeScale(size)
+      this.oldSize = this.size
+    },
+    minusSize() {
+      if (this.size <= 0) return
+      this.size -= 1
+      this.$refs.cropper.changeScale(-1)
+      this.oldSize = this.size
+    },
+    plusSize() {
+      if (this.size >= 20) return
+      this.size += 1
+      this.$refs.cropper.changeScale(1)
+      this.oldSize = this.size
+    },
+    sizeApply() {
+      this.$refs.cropper.getCropBlob((data) => {
+        // do something
+        const file = new File([data], this.fileName)
+        this.$emit('set-avatar', file)
+        this.dialog = false
+      })
+    }
+  }
+}
+</script>
