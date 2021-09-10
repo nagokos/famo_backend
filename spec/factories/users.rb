dd23b3a7873ca03ecf0b74f8dae5b4fdbcae8e97
naohiro <your.email@kosuda0428@icloud.com>
2021-09-11 02:38:43 +0900

    [Remove]不要なコードを削除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :user do
     name { Faker::Name.name }
-    birth_date { Faker::Date.birthday.strftime('%F') }
     email { Faker::Internet.unique.email }
     password { 'Foobar0123' }
   end
