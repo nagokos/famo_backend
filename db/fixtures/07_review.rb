072fb3165071586839b437944ddb5e52fc6c22bd
naohiro <your.email@kosuda0428@icloud.com>
2021-09-14 18:06:09 +0900

    [Fix]初期データの修正

--- db/fixtures/07_review.rb
+++ db/fixtures/07_review.rb
@@ -1,9 +1,10 @@
 reviewer = User.find_by(email: 'rails@gmail.com')
 reviewee = User.where(role: 'player').first
-content = 'とても良いプレーでした。次戦も期待しています。' * 10
+content = 'とても良いプレーでした。次戦も期待しています。' * 15
 
 100.times do |index|
-  reviewer.active_reviews.create!(
+  Review.seed(
+    reviewer_id: reviewer.id,
     reviewee_id: reviewee.id,
     game_date: Time.current.ago(index.days),
     rate: format('%10.1f', rand(1.0..5.0)).strip.to_f,
