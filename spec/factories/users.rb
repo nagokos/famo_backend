9eab3d22586b8de4625c06f312b99ea89a8ba36c
naohiro <your.email@kosuda0428@icloud.com>
2021-09-15 17:18:37 +0900

    [Add]trait

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -11,5 +11,25 @@ FactoryBot.define do
 
   trait :player do
     role { 'player' }
+    birth_date { Time.current.ago(16.years) }
+    after(:build) do |user|
+      create(:profile, rate: 3.0, user: user)
+    end
+  end
+
+  trait :high_rate do
+    role { 'player' }
+    birth_date { Time.current.ago(16.years) }
+    after(:build) do |user|
+      create(:profile, rate: 5.0, user: user)
+    end
+  end
+
+  trait :low_rate do
+    role { 'player' }
+    birth_date { Time.current.ago(16.years) }
+    after(:build) do |user|
+      create(:profile, rate: 1.0, user: user)
+    end
   end
 end
