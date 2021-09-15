d1e21d9b84072947f9d437a9849b4a8b8fe560ea
naohiro <your.email@kosuda0428@icloud.com>
2021-09-15 17:09:31 +0900

    [Fix]レスポンスの修正

--- app/controllers/api/v1/users/game_dates_controller.rb
+++ app/controllers/api/v1/users/game_dates_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::Users::GameDatesController < Api::V1::BaseController
   def index
     user = User.find(params[:user_id])
     game_dates = user.role_and_game_date_reviews.published.map(&:game_date).map(&:strftime)
-    render json: { reviews: game_dates }
+    render json: { dates: game_dates }
   end
 end
