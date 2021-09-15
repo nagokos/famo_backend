553e51ed31797e0298d558c685a82ebc430b58b0
naohiro <your.email@kosuda0428@icloud.com>
2021-09-16 01:42:50 +0900

    [Fix]エンドポイントの変更

--- spec/requests/api/v1/users/rating_averages_spec.rb
+++ spec/requests/api/v1/users/rating_averages_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe "Api::V1::Users::RatingAverages", type: :request do
   let!(:reviewer) { create(:user) }
   let!(:reviewee) { create(:user, :player_student) }
   before { create_list(:review, 10, reviewer: reviewer, reviewee: reviewee) }
-  describe "GET /api/v1/users/:user_id/rating_average" do
+  describe "GET /api/v1/users/:user_id/rating_averages" do
     it '評価点の平均を返す' do
-      get "/api/v1/users/#{reviewer.id}/rating_average"
+      get "/api/v1/users/#{reviewer.id}/rating_averages"
       expect(json).to_not eq(0)
     end
 
     context 'レビューが一件もない' do
-      before { get "/api/v1/users/#{reviewee.id}/rating_average" }
+      before { get "/api/v1/users/#{reviewee.id}/rating_averages" }
       it '０を返す' do
         expect(json).to eq(0)
       end
