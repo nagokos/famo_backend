16cd4fc989a6916f4f7cc126907946237a4e0281
naohiro <your.email@kosuda0428@icloud.com>
2021-10-02 10:25:40 +0900

    [Fix]不要なコードを修正

--- config/puma.rb
+++ config/puma.rb
@@ -42,8 +42,6 @@ pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
 
-app_root = File.expand_path('..', __dir__)
+app_root = File.expand_path('../..', __FILE__)
 bind "unix://#{app_root}/tmp/sockets/puma.sock"
-unless ENV.fetch('RAILS_ENV', 'development') == 'development'
-  stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
-end
+stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
