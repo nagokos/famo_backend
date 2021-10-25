8089f97a0e0f54e01de425b24cf370cee7641584
naohiro <your.email@kosuda0428@icloud.com>
2021-10-25 18:09:20 +0900

    [Add]associationを追加

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,8 @@ class Review < ApplicationRecord
   belongs_to :reviewer, class_name: 'User'
   belongs_to :reviewee, class_name: 'User'
 
+  has_many :likes, dependent: :destroy
+
   validates :rate, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
   validates :game_date, presence: true, format: { with: /\d{4}-\d{2}-\d{2}/ }, uniqueness: { scope: %i[reviewer reviewee] }
   validates :privacy, presence: true
