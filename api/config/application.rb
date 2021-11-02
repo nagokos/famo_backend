8dffa5ef928ad5b52551a7f27dd1efc4fe2d50f7
naohiro <your.email@kosuda0428@icloud.com>
2021-11-02 13:45:13 +0900

    [Add]クッキーを使用できるように設定

--- api/config/application.rb
+++ api/config/application.rb
@@ -49,5 +49,8 @@ module Myapp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.api_only = true
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+    config.middleware.use ActionDispatch::ContentSecurityPolicy::Middleware
   end
 end
