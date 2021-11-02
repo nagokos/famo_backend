781fe4494cd75131ba98e13251ddb3f8c3edbb9a
naohiro <your.email@kosuda0428@icloud.com>
2021-11-02 13:43:12 +0900

    [Add]クッキーが使えるように設定

--- api/app/controllers/api/v1/base_controller.rb
+++ api/app/controllers/api/v1/base_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::BaseController < ApplicationController
   before_action :check_xhr_header, only: %i[create update destroy]
   after_action { pagy_headers_merge(@pagy) if @pagy }
-
+  
   include Pagy::Backend
+  include ActionController::Cookies
   include ErrorHandle
 end
